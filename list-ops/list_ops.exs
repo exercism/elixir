10ae9265b90dc8042a5fd1f2626d7e0648f4cb29
Henrik Nyh <henrik@nyh.se>
2014-11-07 21:39:58 +0100

    Cleanup: trailing whitespace, filename, temp file

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -2,18 +2,18 @@ defmodule ListOps do
   # Please don't use any external modules (especially List) in your
   # implementation. The point of this exercise is to create these basic functions
   # yourself.
-  # 
+  #
   # Note that `++` is a function from an external module (Kernel, which is
   # automatically imported) and so shouldn't be used either.
- 
+
   @spec count(list) :: non_neg_integer
   def count(l) do
-    
+
   end
 
   @spec reverse(list) :: list
   def reverse(l) do
-    
+
   end
 
   @spec map(list, (any -> any)) :: list
