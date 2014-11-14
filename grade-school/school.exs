88970620c481eea458512f3bffc6bd741a2cce33
Aaron Bull Schaefer <aaron@elasticdog.com>
2014-11-14 10:45:54 -0800

    Switch grade-school from tabs to spaces

--- grade-school/school.exs
+++ grade-school/school.exs
@@ -1,31 +1,31 @@
 defmodule School do
-	@moduledoc """
-	Simulate students in a school.
+  @moduledoc """
+  Simulate students in a school.
 
-	Each student is in a grade.
-	"""
+  Each student is in a grade.
+  """
 
-	@doc """
-	Add a student to a particular grade in school.
-	"""
-	@spec add(Dict.t, string, pos_integer) :: Dict.t
-	def add(db, name, grade) do
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(Dict.t, string, pos_integer) :: Dict.t
+  def add(db, name, grade) do
 
-	end
+  end
 
-	@doc """
-	Return the names of the students in a particular grade.
-	"""
-	@spec grade(Dict.t, pos_integer) :: [String]
-	def grade(db, grade) do
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(Dict.t, pos_integer) :: [String]
+  def grade(db, grade) do
 
-	end
+  end
 
-	@doc """
-	Sorts the school by grade and name.
-	"""
-	@spec sort(Dict) :: Dict.t
-	def sort(db) do
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(Dict) :: Dict.t
+  def sort(db) do
 
-	end
+  end
 end
