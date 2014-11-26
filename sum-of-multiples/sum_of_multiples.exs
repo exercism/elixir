bcacc2e9039a4b1828fe45f33300d75b94436b6a
Aaron Bull Schaefer <aaron@elasticdog.com>
2014-11-25 16:04:09 -0800

    Fix the incorrect slashes denoting a default value in sum-of-multiples

--- sum-of-multiples/sum_of_multiples.exs
+++ sum-of-multiples/sum_of_multiples.exs
@@ -6,7 +6,7 @@ defmodule SumOfMultiples do
   The default factors are 3 and 5.
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
-  def to(limit, factors // [3, 5]) do
+  def to(limit, factors \\ [3, 5]) do
   
   end
 
