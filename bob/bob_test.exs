9f3eb55ca104e7484df7ba6525e023936180e2cd
GHajba <hajbag@gmail.com>
2015-05-26 08:19:11 +0200

    bob: tag tests pending

--- bob/bob_test.exs
+++ bob/bob_test.exs
@@ -5,6 +5,7 @@ else
 end
 
 ExUnit.start
+ExUnit.configure(exclude: :pending)
 
 defmodule TeenagerTest do
   use ExUnit.Case, async: true
@@ -13,56 +14,69 @@ defmodule TeenagerTest do
     assert Teenager.hey("Tom-ay-to, tom-aaaah-to.") == "Whatever."
   end
 
+  @tag :pending
   test "shouting" do
-    # assert Teenager.hey("WATCH OUT!") == "Whoa, chill out!"
+    assert Teenager.hey("WATCH OUT!") == "Whoa, chill out!"
   end
 
+  @tag :pending
   test "asking a question" do
-    # assert Teenager.hey("Does this cryogenic chamber make me look fat?") == "Sure."
+    assert Teenager.hey("Does this cryogenic chamber make me look fat?") == "Sure."
   end
 
+  @tag :pending
   test "talking forcefully" do
-    # assert Teenager.hey("Let's go make out behind the gym!") == "Whatever."
+    assert Teenager.hey("Let's go make out behind the gym!") == "Whatever."
   end
 
+  @tag :pending
   test "talking in capitals" do
-    # assert Teenager.hey("This Isn't Shouting!") == "Whatever."
+    assert Teenager.hey("This Isn't Shouting!") == "Whatever."
   end
 
+  @tag :pending
   test "shouting numbers" do
-    # assert Teenager.hey("1, 2, 3 GO!") == "Whoa, chill out!"
+    assert Teenager.hey("1, 2, 3 GO!") == "Whoa, chill out!"
   end
 
+  @tag :pending
   test "shouting with special characters" do
-    # assert Teenager.hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") == "Whoa, chill out!"
+    assert Teenager.hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") == "Whoa, chill out!"
   end
 
+  @tag :pending
   test "shouting with no exclamation mark" do
-    # assert Teenager.hey("I HATE YOU") == "Whoa, chill out!"
+    assert Teenager.hey("I HATE YOU") == "Whoa, chill out!"
   end
 
+  @tag :pending
   test "statement containing question mark" do
-    # assert Teenager.hey("Ending with ? means a question.") == "Whatever."
+    assert Teenager.hey("Ending with ? means a question.") == "Whatever."
   end
 
+  @tag :pending
   test "silence" do
-    # assert Teenager.hey("") == "Fine. Be that way!"
+    assert Teenager.hey("") == "Fine. Be that way!"
   end
 
+  @tag :pending
   test "prolonged silence" do
-    # assert Teenager.hey("  ") == "Fine. Be that way!"
+    assert Teenager.hey("  ") == "Fine. Be that way!"
   end
 
+  @tag :pending
   test "only numbers" do
-    # assert Teenager.hey("1, 2, 3") == "Whatever."
+    assert Teenager.hey("1, 2, 3") == "Whatever."
   end
 
+  @tag :pending
   test "question with numbers" do
-    # assert Teenager.hey("4?") == "Sure."
+    assert Teenager.hey("4?") == "Sure."
   end
 
+  @tag :pending
   test "shouting in Russian" do
     # Hopefully this is Russian for "get out"
-    # assert Teenager.hey("УХОДИТЬ") == "Whoa, chill out!"
+    assert Teenager.hey("УХОДИТЬ") == "Whoa, chill out!"
   end
 end
