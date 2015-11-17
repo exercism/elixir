0b77b05fd7a97b7819f29c0d32e436e600092607
Rob Phoenix <robertphoenix@gmail.com>
2015-11-17 15:20:45 +0000

    Add hello-world exercise to Elixir track
    The Hello, World! program is a classic introductory programming
    exercise.  It hopefully provides a slightly gentler start to
    learning Elixir, and introduces the Exercism concept to new users.

---
+++ hello-world/hello_world.exs
@@ -0,0 +1,7 @@
+defmodule HelloWorld do
+  def hello(name) do
+
+    "Your implementation goes here"
+
+  end
+end
---
+++ hello-world/hello_world_test.exs
@@ -0,0 +1,27 @@
+if System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("example.exs")
+else
+  Code.load_file("hello_world.exs")
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule HelloWorldTest do
+  use ExUnit.Case, async: true
+
+  test "says hello with no name" do
+    assert HelloWorld.hello() == "Hello, World!"
+  end
+
+  @tag :pending
+  test "says hello sample name" do
+    assert HelloWorld.hello("Alice") == "Hello, Alice!"
+  end
+
+  @tag :pending
+  test "says hello other sample name" do
+    assert HelloWorld.hello("Bob") == "Hello, Bob!"
+  end
+
+end
