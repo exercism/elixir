fcc18eb275aef2db62b798d5edadae5b4c824796
Lew Parker <parker@earthvectors.com>
2015-12-09 17:13:03 -0700

    Update gigasecond per issue #35

--- gigasecond/example.exs
+++ gigasecond/example.exs
@@ -1,7 +1,6 @@
 defmodule Gigasecond do
-  def from(date) do
-    seconds = :calendar.datetime_to_gregorian_seconds({date, {0, 0, 0}})
-    {result, {_, _, _}} = :calendar.gregorian_seconds_to_datetime(seconds + 1000000000)
-    result
+  def from(time) do
+    seconds = :calendar.datetime_to_gregorian_seconds(time)
+    :calendar.gregorian_seconds_to_datetime(seconds + 1000000000)
   end
 end
--- gigasecond/gigasecond.exs
+++ gigasecond/gigasecond.exs
@@ -2,9 +2,9 @@ defmodule Gigasecond do
 	@doc """
 	Calculate a date one billion seconds after an input date.
 	"""
-	@spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
+	@spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
 
-	def from({year, month, day}) do
+	def from({{year, month, day}, {hours, minutes, seconds}}) do
 
 	end
 end
