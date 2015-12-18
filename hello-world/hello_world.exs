2fa6b9c34adf0e6c5b5e277e1b363dea6f53112d
George Hudson <georgehudson78@gmail.com>
2015-12-18 16:48:14 -0700

    added some comments within the def hello(name) function to make it more clear to novice elixir users why the first test would be failing.
    changed the word method to function

--- hello-world/hello_world.exs
+++ hello-world/hello_world.exs
@@ -2,6 +2,20 @@ defmodule HelloWorld do
   def hello(name) do
 
     "Your implementation goes here"
+    # Elixir counts the number of arguments as part of the function name.
+    # For instance,
+
+      #def hello() do
+      #end
+
+    # would be a completely different function from
+
+      #def hello(name) do
+      #end
+
+    # Can you find a way to make all the tests pass with just one function?
+    # Hint: look into argument defaults here:
+    # http://elixir-lang.org/getting-started/modules.html#default-arguments
 
   end
 end
