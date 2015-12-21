19d311ded7325a8744f2d1cb93ecdfd7f5fdede3
George Hudson <georgehudson78@gmail.com>
2015-12-20 19:36:45 -0700

    added in comments for beginning user clarity of functions being different with different arguments
    moved the comments above the function instead of inside based upon feedback
    
    removed extra empty line

--- hello-world/hello_world.exs
+++ hello-world/hello_world.exs
@@ -1,4 +1,20 @@
 defmodule HelloWorld do
+
+  # Elixir counts the number of arguments as part of the function name.
+  # For instance,
+
+    #def hello() do
+    #end
+
+  # would be a completely different function from
+
+    #def hello(name) do
+    #end
+
+  # Can you find a way to make all the tests pass with just one function?
+  # Hint: look into argument defaults here:
+  # http://elixir-lang.org/getting-started/modules.html#default-arguments
+
   def hello(name) do
 
     "Your implementation goes here"
