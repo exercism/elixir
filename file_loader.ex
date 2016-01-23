a0a3f84464c6cc9d5e1a3dcde88f503b5b0c68a5
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-23 11:22:19 +0100

    define module FileLoader if not yet defined
    problem: 
    - many warning when the "file_loader" file is loaded multiple times
    
    solution: 
    - check if the module is already defined and do it only if not

--- file_loader.ex
+++ file_loader.ex
@@ -1,12 +1,14 @@
-defmodule FileLoader do
-  def load(name, dir) do
-    if System.get_env("EXERCISM_TEST_EXAMPLES") do
-      Code.load_file("example.exs", dir)
-    else
-      Code.load_file("#{name}.exs", dir)
-    end
+unless Code.ensure_loaded?(FileLoader) do
+  defmodule FileLoader do
+    def load(name, dir) do
+      if System.get_env("EXERCISM_TEST_EXAMPLES") do
+        Code.load_file("example.exs", dir)
+      else
+        Code.load_file("#{name}.exs", dir)
+      end
 
-    ExUnit.start
-    ExUnit.configure exclude: :pending, trace: true
+      ExUnit.start
+      ExUnit.configure exclude: :pending, trace: true
+    end
   end
 end
