faa8994adce712d1abf6ef07e65eaef4bfd6f3ba
Rob Phoenix <robertphoenix@gmail.com>
2016-02-01 13:21:15 +0000

    Add typespecs to Hello World exercise, move function help into moduledoc.

--- hello-world/hello_world.exs
+++ hello-world/hello_world.exs
@@ -1,20 +1,28 @@
 defmodule HelloWorld do
+  @moduledoc """
+  Elixir counts the number of arguments as part of the function name.
+  For instance;
 
-  # Elixir counts the number of arguments as part of the function name.
-  # For instance,
+      def hello() do
+      end
 
-    #def hello() do
-    #end
+  would be a completely different function from
 
-  # would be a completely different function from
+      def hello(name) do
+      end
 
-    #def hello(name) do
-    #end
+  Can you find a way to make all the tests pass with just one
+  function?
 
-  # Can you find a way to make all the tests pass with just one function?
-  # Hint: look into argument defaults here:
-  # http://elixir-lang.org/getting-started/modules.html#default-arguments
+  Hint: look into argument defaults here:
+  http://elixir-lang.org/getting-started/modules.html#default-arguments
+  """
 
+  @doc """
+  Greets the user by name, or by saying "Hello, World!"
+  if no name is given.
+  """
+  @spec hello(String.t) :: String.t
   def hello(name) do
 
     "Your implementation goes here"
