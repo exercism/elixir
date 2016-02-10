8f738e4f57a0250fd78c5a20c6572fa5052a2327
Tyler Long <tyler.stephen.long@gmail.com>
2016-02-10 16:07:37 -0700

    Adds the acronym exercise

---
+++ exercises/acronym/acronym.exs
@@ -0,0 +1,9 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string. 
+  "This is a string" => "TIAS"
+  """
+  def abbreviate(string) do
+   
+  end
+end
---
+++ exercises/acronym/example.exs
@@ -0,0 +1,10 @@
+defmodule Acronym do
+
+  def abbreviate(string) do
+    Regex.scan(~r/[A-Z]+[a-z]*|[a-z]+/, string)
+    |> List.flatten
+    |> Enum.map(fn(x) -> String.first(x) end)
+    |> Enum.join("")
+    |> String.upcase
+  end
+end
