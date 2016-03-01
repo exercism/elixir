5648c33704d798001025fe5f82212b6a2c73803f
Alex Jensen <96dalex@gmail.com>
2016-03-01 11:26:42 -0700

    add queen attack exercise

---
+++ exercises/queen-attack/example.exs
@@ -0,0 +1,51 @@
+defmodule Queens do
+  @type t :: %Queens{ white: {integer, integer}, black: {integer, integer} }
+  defstruct white: {0, 3}, black: {7, 3}
+
+  @doc """
+  Creates a new set of Queens
+  """
+  @spec new(nil | list) :: Queens.t()
+  def new(white: same_loc, black: same_loc), do: raise ArgumentError
+  def new(white: white, black: black), do: %Queens{white: white, black: black}
+  def new, do: %Queens{}
+
+  @doc """
+  Gives a string reprentation of the board with
+  white and black queen locations shown
+  """
+  @spec to_string(Queens.t()) :: String.t()
+  def to_string(%Queens{ white: white, black: black }) do
+    generate_board
+    |> insert_queen(white, "W")
+    |> insert_queen(black, "B")
+    |> Enum.map(&Enum.join(&1, " "))
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Checks if the queens can attack each other
+  """
+  @spec can_attack?(Queens.t()) :: boolean
+  def can_attack?(%Queens{ white: white, black: black }) do
+    {white_x, white_y} = white
+    {black_x, black_y} = black
+    white_x == black_x || white_y == black_y || diagonal?(white, black)
+  end
+
+  defp diagonal?({x1, y1}, {x2, y2}) do
+    abs(x1 - x2) == abs(y1 - y2)
+  end
+
+  defp insert_queen(board, {x, y}, letter) do
+    List.update_at board, x, fn(row) ->
+      List.replace_at row, y, letter
+    end
+  end
+
+  defp generate_board do
+    "_"
+    |> List.duplicate(8)
+    |> List.duplicate(8)
+  end
+end
---
+++ exercises/queen-attack/queen_attack_test.exs
@@ -0,0 +1,123 @@
+if System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("example.exs")
+else
+  Code.load_file("queen_attack.exs")
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule QueenAttackTest do
+  use ExUnit.Case
+
+  # @tag :pending
+  test "default positions" do
+    queens = Queens.new
+    assert queens.white == {0, 3}
+    assert queens.black == {7, 3}
+  end
+
+  @tag :pending
+  test "specific placement" do
+    queens = Queens.new(white: {3, 7}, black: {6, 1})
+    assert queens.white == {3, 7}
+    assert queens.black == {6, 1}
+  end
+
+  @tag :pending
+  test "cannot occupy same space" do
+    assert_raise ArgumentError, fn ->
+      Queens.new(white: {2, 4}, black: {2, 4})
+    end
+  end
+
+  @tag :pending
+  test "string representation" do
+    queens = Queens.new(white: {2, 4}, black: {6, 6})
+    board = String.strip """
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ W _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ B _
+_ _ _ _ _ _ _ _
+    """
+    assert Queens.to_string(queens) == board
+  end
+
+  @tag :pending
+  test "another string representation" do
+    queens = Queens.new(white: {7, 1}, black: {0, 0})
+    board = String.strip """
+B _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ W _ _ _ _ _ _
+    """
+    assert Queens.to_string(queens) == board
+  end
+
+  @tag :pending
+  test "yet another string representation" do
+    queens = Queens.new(white: {4, 3}, black: {3, 4})
+    board = String.strip """
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ B _ _ _
+_ _ _ W _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _
+    """
+    assert Queens.to_string(queens) == board
+  end
+
+  @tag :pending
+  test "cannot attack" do
+    queens = Queens.new(white: {2, 3}, black: {4, 7})
+    refute Queens.can_attack?(queens)
+  end
+
+  @tag :pending
+  test "can attack on same row" do
+    queens = Queens.new(white: {2, 4}, black: {2, 7})
+    assert Queens.can_attack?(queens)
+  end
+
+  @tag :pending
+  test "can attack on same column" do
+    queens = Queens.new(white: {5, 4}, black: {2, 4})
+    assert Queens.can_attack?(queens)
+  end
+
+  @tag :pending
+  test "can attack on diagonal" do
+    queens = Queens.new(white: {1, 1}, black: {6, 6})
+    assert Queens.can_attack?(queens)
+  end
+
+  @tag :pending
+  test "can attack on other diagonal" do
+    queens = Queens.new(white: {0, 6}, black: {1, 7})
+    assert Queens.can_attack?(queens)
+  end
+
+  @tag :pending
+  test "can attack on yet another diagonal" do
+    queens = Queens.new(white: {4, 1}, black: {6, 3})
+    assert Queens.can_attack?(queens)
+  end
+
+  @tag :pending
+  test "can attack on a diagonal slanted the other way" do
+    queens = Queens.new(white: {6, 1}, black: {1, 6})
+    assert Queens.can_attack?(queens)
+  end
+end
