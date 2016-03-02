3b4770b20fcee1f62f8516fdf055af0846139c03
Alex Jensen <96dalex@gmail.com>
2016-03-02 16:24:42 -0700

    add saddle points exercise

---
+++ exercises/saddle-points/saddle_points.exs
@@ -0,0 +1,28 @@
+defmodule Matrix do
+  @doc """
+  Parses a string representation of a matrix
+  to a list of rows
+  """
+  @spec rows(String.t()) :: [[integer]]
+  def rows(str) do
+
+  end
+
+  @doc """
+  Parses a string representation of a matrix
+  to a list of columns
+  """
+  @spec columns(String.t()) :: [[integer]]
+  def columns(str) do
+
+  end
+
+  @doc """
+  Calculates all the saddle points from a string
+  representation of a matrix
+  """
+  @spec saddle_points(String.t()) :: [{integer, integer}]
+  def saddle_points(str) do
+
+  end
+end
