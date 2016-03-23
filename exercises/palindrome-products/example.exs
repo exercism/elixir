daaf2248b356bd313627ddd239454f749019a56a
Dan Swain <dan.t.swain@gmail.com>
2016-03-23 15:47:09 -0400

    Fix map type and make it consistent
    A few places were using `Map` as a type, which technically means the
    atom `:'Elixir.Map'`.  The type should be `map`.  There was also some
    inconsistency of `map` vs `map()`.  It seems that for the most part the
    exercises aren't using `()` when unnecessary, so I changed the existing
    instances of `map()` to just `map`.

--- exercises/palindrome-products/example.exs
+++ exercises/palindrome-products/example.exs
@@ -3,8 +3,8 @@ defmodule Palindromes do
   @doc """
   Generates all palindrome products from an optionally given min factor (or 1) to a given max factor.
   """
-  @spec generate(non_neg_integer) :: map()
-  @spec generate(non_neg_integer, non_neg_integer) :: map()
+  @spec generate(non_neg_integer) :: map
+  @spec generate(non_neg_integer, non_neg_integer) :: map
   def generate(max_factor, min_factor \\ 1) do
     Enum.reduce(min_factor..max_factor, %{}, fn(x, dict) ->
       Enum.reduce(x..max_factor, dict, fn(y, products) ->
