f76e05fbfa40bccd6f6710e8c25b8f06f9814d4e
jimmbraddock <jimmbraddock@gmail.com>
2016-03-27 01:05:01 +0300

    Changing approach of tests: check only result of diamond shape.
    Replace doc of build_shape function.

--- exercises/diamond/diamond.exs
+++ exercises/diamond/diamond.exs
@@ -1,6 +1,7 @@
 defmodule Diamond do
   @doc """
-  List the allergies for which the corresponding flag bit is true.
+  Given a letter, it prints a diamond starting with 'A',
+  with the supplied letter at the widest point.
   """
   @spec build_shape(char) :: String.t
   def build_shape(letter) do
