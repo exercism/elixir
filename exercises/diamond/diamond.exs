9820fd2e6d073c03de786cfdf5004aa11c02e507
jimmbraddock <jimmbraddock@gmail.com>
2016-03-27 00:49:16 +0300

    Add diamond exercise

---
+++ exercises/diamond/diamond.exs
@@ -0,0 +1,9 @@
+defmodule Diamond do
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec build_shape(char) :: String.t
+  def build_shape(letter) do
+
+  end
+end
