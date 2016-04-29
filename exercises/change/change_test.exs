fe1f12e15d0eec3bdd3cf4a87e4c32a9232c2226
Bernardo Chaves <bernardo.amc@gmail.com>
2016-04-29 16:06:56 -0300

    Add change exercise.

---
+++ exercises/change/change_test.exs
@@ -0,0 +1,44 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("change.exs")
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule ChangeTest do
+  use ExUnit.Case
+
+  test "returns :error on empty list" do
+    assert Change.generate(1, []) == :error
+  end
+
+  @tag :pending
+  test "generates the correct change when only one coin type is needed" do
+    change = %{1 => 5, 10 => 0}
+    assert Change.generate(5, [1, 10]) == {:ok, change}
+  end
+
+  @tag :pending
+  test "generates the correct change when multiple coin types are needed" do
+    change = %{1 => 3, 5 => 1, 10 => 1}
+    assert Change.generate(18, [1, 5, 10]) == {:ok, change}
+  end
+
+  @tag :pending
+  test "returns :error when it is not possible to generate change" do
+    assert Change.generate(3, [5, 10, 25]) == :error
+  end
+
+  @tag :pending
+  test "generates the same change given any coin order" do
+    change = %{1 => 3, 5 => 1, 10 => 1}
+    assert Change.generate(18, [1, 5, 10]) == {:ok, change}
+    assert Change.generate(18, [10, 5, 1]) == {:ok, change}
+  end
+
+  @tag :pending
+  test "generates the correct change for large values with many coins" do
+    change = %{1 => 3, 5 => 1, 10 => 0, 25 => 1, 100 => 1}
+    assert Change.generate(133, [1, 5, 10, 25, 100]) == {:ok, change}
+  end
+end
