1ae2ffbb1c7936103a3b4f47fcf3fdf1fd14b9b7
Devon Estes <devon.c.estes@gmail.com>
2016-05-25 10:21:25 -0700

    Add beer-song
    This adds the `beer-song` exercise for Elixir. I've basically copied the
    structure that was in the Ruby exercise, but there may be some changes that
    might be particularly applicable for Elixir - I'd be happy to incorporate them
    if anyone has any feedback!

---
+++ exercises/beer-song/beer_song.exs
@@ -0,0 +1,17 @@
+defmodule BeerSong do
+  @doc """
+  Get a single verse of the beer song
+  """
+  @spec verse(integer) :: String.t
+  def verse(number) do
+    # Your implementation here...
+  end
+
+  @doc """
+  Get the entire beer song for a given range of numbers of bottles.
+  """
+  @spec lyrics(Range.t) :: String.t
+  def lyrics(range) do
+    # Your implementation here...
+  end
+end
