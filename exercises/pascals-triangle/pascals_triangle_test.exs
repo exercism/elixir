3a801c1d2a3f734d8ed86a87074659fe1eb107eb
Alex <96dalex@gmail.com>
2016-06-26 18:14:29 -0600

    PR comments

--- exercises/pascals-triangle/pascals_triangle_test.exs
+++ exercises/pascals-triangle/pascals_triangle_test.exs
@@ -9,36 +9,37 @@ ExUnit.configure exclude: :pending, trace: true
 defmodule PascalsTriangleTest do
   use ExUnit.Case
 
-  test "test one row" do
-    assert [[1]] == PascalsTriangle.rows(1)
+  # @tag pending
+  test "one row" do
+    assert PascalsTriangle.rows(1) == [[1]]
   end
 
   @tag :pending
-  test "test two rows" do
-    assert [[1], [1, 1]] == PascalsTriangle.rows(2)
+  test "two rows" do
+    assert PascalsTriangle.rows(2) == [[1], [1, 1]]
   end
 
   @tag :pending
-  test "test three rows" do
-    assert [[1], [1, 1], [1, 2, 1]] == PascalsTriangle.rows(3)
+  test "three rows" do
+    assert PascalsTriangle.rows(3) == [[1], [1, 1], [1, 2, 1]]
   end
 
   @tag :pending
-  test "test fourth row" do
-    assert [1, 3, 3, 1] == List.last(PascalsTriangle.rows(4))
+  test "fourth row" do
+    assert List.last(PascalsTriangle.rows(4)) ==  [1, 3, 3, 1]
   end
 
   @tag :pending
-  test "test fifth row" do
-    assert [1, 4, 6, 4, 1] == List.last(PascalsTriangle.rows(5))
+  test "fifth row" do
+    assert List.last(PascalsTriangle.rows(5)) ==  [1, 4, 6, 4, 1]
   end
 
   @tag :pending
-  test "test twentieth row" do
+  test "twentieth row" do
     expected = [
       1, 19, 171, 969, 3876, 11_628, 27_132, 50_388, 75_582, 92_378, 92_378,
       75_582, 50_388, 27_132, 11_628, 3876, 969, 171, 19, 1
     ]
-    assert expected == List.last(PascalsTriangle.rows(20))
+    assert List.last(PascalsTriangle.rows(20)) ==  expected
   end
 end
