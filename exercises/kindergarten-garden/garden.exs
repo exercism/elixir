e27c7e3d324b4aad7d18642fa1643f263faf81a9
Devon Estes <devon.c.estes@gmail.com>
2016-07-20 14:20:55 -0700

    Add kindergarten-garden exercise
    This adds the kindergarten garden exercise. There wasn't a set of example data
    that we had in the x-common repo, so I chose to mimic the tests from the Ruby
    implementation (i.e. adding the ability to pass in custom names, which wasn't
    expressly stated in the Readme).
    
    I'm hoping that the tests that I set up are generic enough to offer various
    ways of solving this problem and that they're not too prescriptive.

---
+++ exercises/kindergarten-garden/garden.exs
@@ -0,0 +1,14 @@
+defmodule Garden do
+  @doc """
+    Accepts a string representing the arrangement of cups on a windowsill and a
+    list with names of students in the class. The student names list does not
+    have to be in alphabetical order.
+
+    It decodes that string into the various gardens for each student and returns
+    that information in a map.
+  """
+
+  @spec info(String.t(), list) :: map
+  def info(info_string, student_names) do
+  end
+end
