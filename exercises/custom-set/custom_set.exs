faf6f193d18d1d06def19b4097b5734786e780f2
Devon Estes <devon.c.estes@gmail.com>
2016-08-22 07:07:33 -0700

    Rewrite tests and example for CustomSet (#226)
    We have a lot of issues open around the `CustomSet` example. This PR rewrites
    that to remove the deprecated `Set` module, and also updates it to use the
    current tests listed in the `exercism/x-common` repo.
    
    This should address Issues #121, #180, #189, #198 and #217.

--- exercises/custom-set/custom_set.exs
+++ exercises/custom-set/custom_set.exs
@@ -1,5 +1,43 @@
 defmodule CustomSet do
-  # This lets the compiler check that all Set callback functions have been
-  # implemented.
-  @behaviour Set
+  @opaque t :: %__MODULE__{map: map}
+
+  @spec new(Enum.t) :: t
+  def new(enumerable) do
+  end
+
+  @spec empty?(t) :: boolean
+  def empty?(custom_set) do
+  end
+
+  @spec contains?(t, any) :: boolean
+  def contains?(custom_set, element) do
+  end
+
+  @spec subset?(t, t) :: boolean
+  def subset?(custom_set_1, custom_set_2) do
+  end
+
+  @spec disjoint?(t, t) :: boolean
+  def disjoint?(custom_set_1, custom_set_2) do
+  end
+
+  @spec equal?(t, t) :: boolean
+  def equal?(custom_set_1, custom_set_2) do
+  end
+
+  @spec add(t, any) :: t
+  def add(custom_set, element) do
+  end
+
+  @spec intersection(t, t) :: t
+  def intersection(custom_set_1, custom_set_2) do
+  end
+
+  @spec difference(t, t) :: t
+  def difference(custom_set_1, custom_set_2) do
+  end
+
+  @spec union(t, t) :: t
+  def union(custom_set_1, custom_set_2) do
+  end
 end
