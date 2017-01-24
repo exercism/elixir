2c0b07d16ab109d5a502558abea89c8ed698bf11
Mr Rogers <jon@bunnymatic.com>
2017-01-24 13:31:07 -0800

    update example to match new test cases

--- exercises/grains/example.exs
+++ exercises/grains/example.exs
@@ -1,5 +1,6 @@
 defmodule Grains do
   use Bitwise, only_operators: true
-  def square(number), do: 1 <<< (number - 1)
+  def square(number) when number in (1..64), do: 1 <<< (number - 1)
+  def square(_), do: raise ArgumentError, message: "The requested square must be between 1 and 64 (inclusive )"
   def total, do: Enum.reduce(1..64, 0, fn(n, acc) -> acc + square(n) end)
 end
