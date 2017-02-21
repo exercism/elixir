e3fc242c06d12cba4025b895ff4ece06ad4642ea
DoggettCK <doggettc@gmail.com>
2017-02-21 07:00:20 -0600

    Add exercise 'series' (#294)
    * Add exercise 'series'
    
    * Made tests clearer, added Unicode test
    
    * Fix typespecs
    
    * Add commented-out pending tag

---
+++ exercises/series/series.exs
@@ -0,0 +1,11 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_s, _size) do
+  end
+end
+
