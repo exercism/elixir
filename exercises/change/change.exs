0e7129bc01c25f1b50173bbd6879865abd2b76ec
Manuel <manuel.albarran@cabify.com>
2017-03-03 19:12:37 +0100

    Updated change exercise to adjust to canonical data.
    There were some tests that differ in many ways with canonical data, like "change with Lower Elbonia Coins". That can't pass with the previous example.exs.
    
    Furthermore, the results of canonical data are implemented with a list, not with a hash / map.

--- exercises/change/change.exs
+++ exercises/change/change.exs
@@ -2,21 +2,21 @@ defmodule Change do
   @doc """
     Determine the least number of coins to be given to the user such
     that the sum of the coins' value would equal the correct amount of change.
-    It returns :error if it is not possible to compute the right amount of coins.
-    Otherwise returns the tuple {:ok, map_of_coins}
+    It returns {:error, "cannot change"} if it is not possible to compute the
+    right amount of coins. Otherwise returns the tuple {:ok, list_of_coins}
 
     ## Examples
 
-      iex> Change.generate(3, [5, 10, 15])
-      :error
+      iex> Change.generate([5, 10, 15], 3)
+      {:error, "cannot change"}
 
-      iex> Change.generate(18, [1, 5, 10])
-      {:ok, %{1 => 3, 5 => 1, 10 => 1}}
+      iex> Change.generate([1, 5, 10], 18)
+      {:ok, [1, 1, 1, 5, 10]}
 
   """
 
-  @spec generate(integer, list) :: {:ok, map} | :error
-  def generate(amount, values) do
+  @spec generate(list, integer) :: {:ok, list} | {:error, String.t}
+  def generate(coins, target) do
 
   end
 end
--- exercises/change/example.exs
+++ exercises/change/example.exs
@@ -2,35 +2,38 @@ defmodule Change do
   @doc """
     Determine the least number of coins to be given to the user such
     that the sum of the coins' value would equal the correct amount of change.
-    It returns :error if it is not possible to compute the right amount of coins.
-    Otherwise returns the tuple {:ok, map_of_coins}
+    It returns {:error, "cannot change"} if it is not possible to compute the
+    right amount of coins. Otherwise returns the tuple {:ok, list_of_coins}
 
     ## Examples
 
-      iex> Change.generate(3, [5, 10, 15])
-      :error
+      iex> Change.generate([5, 10, 15], 3)
+      {:error, "cannot change"}
 
-      iex> Change.generate(18, [1, 5, 10])
-      {:ok, %{1 => 3, 5 => 1, 10 => 1}}
+      iex> Change.generate([1, 5, 10], 18)
+      {:ok, [1, 1, 1, 5, 10]}
 
   """
 
-  @spec generate(integer, list) :: {:ok, map} | :error
-  def generate(amount, values) do
-    values |> Enum.sort(&(&2 < &1)) |> generate(amount, %{})
+  @spec generate(list, integer) :: {:ok, list} | {:error, String.t}
+  def generate(coins, target) do
+    coins |> Enum.sort(&>/2) |> generate(target, [], {:error, "cannot change"})
   end
 
-  defp generate(_, 0, acc), do: {:ok, acc}
-  defp generate([], _, _), do: :error
-  defp generate(values = [h | t], amount, acc) do
-    if amount >= h && divisible?(amount - h, values) do
-      generate(values, amount - h, Map.update(acc, h, 1, &(&1 + 1)))
-    else
-      generate(t, amount, Map.put_new(acc, h, 0))
-    end
+  defp generate(_, _, current, {:ok, best}) when length(current) >= length(best) do
+    {:ok, best}
   end
-
-  defp divisible?(num, values) do
-    Enum.any?(values, &(rem(num, &1) == 0))
+  defp generate(_, 0, current, _) do
+    {:ok, current}
+  end
+  defp generate([], _, _, best) do
+    best
+  end
+  defp generate([coin | coins], target, current, best) when coin > target do
+    generate(coins, target, current, best)
+  end
+  defp generate([coin | coins], target, current, best) do
+    first_try = generate([coin | coins], target - coin, [coin | current], best)
+    generate(coins, target, current, first_try)
   end
 end
