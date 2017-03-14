93023194ccf7450438df8652c051a5622027a13e
Brian Carrigan <brian.carrigan@smashingboxes.com>
2017-03-14 19:57:47 -0400

    implements the exercise 'Ocr Numbers' (#300)
    * dibs: I will implement exercise 'Ocr Numbers'
    
    * Added tests and contract for OCR Numbers
    
    * Finish example solution for ocr-numbers
    
    * Add ocr-numbers for config.json
    
    * Update test do be more Elixir-matic
    
    * Add corrections per feedback in #300.
    
    * Fix unused variable error.
    
    * Correct typos.

---
+++ exercises/ocr-numbers/example.exs
@@ -0,0 +1,47 @@
+defmodule OCRNumbers do
+
+  @doc """
+  Given a 3 x 4 grid of pipes, underscores, and spaces, determine which number is represented, or
+  whether it is garbled.
+  """
+  @spec convert([String.t]) :: String.t
+  def convert(input) do
+    Enum.chunk(input, 4)
+    |> Enum.map(fn row_set -> _convert(row_set, "") end)
+    |> format_output()
+  end
+
+  def format_output([]), do: {:error, 'invalid line count'}
+  def format_output(rows), do: _format_output(Enum.any?(rows, &_error?/1), rows)
+  def _format_output(true, rows), do: Enum.find(rows, &_error?/1)
+  def _format_output(false, output), do: {:ok, Enum.join(output, ",")}
+  def _error?({:error, _}), do: true
+  def _error?(_), do: false
+
+  def _convert(_, {:error, _} = error), do: error
+  def _convert(input, _) when length(input) != 4, do: {:error, 'invalid line count'}
+  def _convert(["", "", "", ""], output), do: output
+  def _convert(input, output) do
+    split_strings = Enum.map(input, fn a -> String.split_at(a, 3) end)
+    this_character = Enum.map(split_strings, fn ({a, _}) -> a end)
+    other_characters = Enum.map(split_strings, fn {_, a} -> a end)
+    lengths = Enum.map(this_character, fn a -> String.length(a) end)
+
+    _convert(other_characters, update_output(lengths, this_character, output))
+  end
+
+  def update_output([3, 3, 3, 3], chars, output), do: output <> recognize_character(chars)
+  def update_output(_, _, _), do: {:error, 'invalid column count'}
+
+  def recognize_character([" _ ", "| |", "|_|", "   "]), do: "0"
+  def recognize_character(["   ", "  |", "  |", "   "]), do: "1"
+  def recognize_character([" _ ", " _|", "|_ ", "   "]), do: "2"
+  def recognize_character([" _ ", " _|", " _|", "   "]), do: "3"
+  def recognize_character(["   ", "|_|", "  |", "   "]), do: "4"
+  def recognize_character([" _ ", "|_ ", " _|", "   "]), do: "5"
+  def recognize_character([" _ ", "|_ ", "|_|", "   "]), do: "6"
+  def recognize_character([" _ ", "  |", "  |", "   "]), do: "7"
+  def recognize_character([" _ ", "|_|", "|_|", "   "]), do: "8"
+  def recognize_character([" _ ", "|_|", " _|", "   "]), do: "9"
+  def recognize_character(_), do: "?"
+end
---
+++ exercises/ocr-numbers/ocr_numbers_test.exs
@@ -0,0 +1,254 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("ocr_numbers.exs", __DIR__)
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule OCRNumbersTest do
+  use ExUnit.Case
+
+  # @tag :pending
+  test "Recognizes 0" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "| |",
+        "|_|",
+        "   "
+      ]
+    )
+    assert number == {:ok, "0"}
+  end
+
+  @tag :pending
+  test "Recognizes 1" do
+    number = OCRNumbers.convert(
+      [
+        "   ",
+        "  |",
+        "  |",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "1"}
+  end
+
+  @tag :pending
+  test "Unreadable but correctly sized inputs return ?" do
+    number = OCRNumbers.convert(
+      [
+        "   ",
+        "  _",
+        "  |",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "?"}
+  end
+
+  @tag :pending
+  test "Input with a number of lines that is not a multiple of four raises an error" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "| |",
+        "   "
+      ]
+    )
+
+    assert number == {:error, 'invalid line count'}
+  end
+
+  @tag :pending
+  test "Input with a number of columns that is not a multiple of three raises an error" do
+    number = OCRNumbers.convert(
+      [
+        "    ",
+        "   |",
+        "   |",
+        "    "
+      ]
+    )
+
+    assert number == {:error, 'invalid column count'}
+  end
+
+  @tag :pending
+  test "Recognizes 110101100" do
+    number = OCRNumbers.convert(
+      [
+        "       _     _        _  _ ",
+        "  |  || |  || |  |  || || |",
+        "  |  ||_|  ||_|  |  ||_||_|",
+        "                           "
+      ]
+    )
+
+    assert number == {:ok, "110101100"}
+  end
+
+  @tag :pending
+  test "Garbled numbers in a string are replaced with ?" do
+    number = OCRNumbers.convert(
+      [
+        "       _     _           _ ",
+        "  |  || |  || |     || || |",
+        "  |  | _|  ||_|  |  ||_||_|",
+        "                           "
+      ]
+    )
+
+    assert number == {:ok, "11?10?1?0"}
+  end
+
+  @tag :pending
+  test "Recognizes 2" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        " _|",
+        "|_ ",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "2"}
+  end
+
+  @tag :pending
+  test "Recognizes 3" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        " _|",
+        " _|",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "3"}
+  end
+
+  @tag :pending
+  test "Recognizes 4" do
+    number = OCRNumbers.convert(
+      [
+        "   ",
+        "|_|",
+        "  |",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "4"}
+  end
+
+  @tag :pending
+  test "Recognizes 5" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "|_ ",
+        " _|",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "5"}
+  end
+
+  @tag :pending
+  test "Recognizes 6" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "|_ ",
+        "|_|",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "6"}
+  end
+
+  @tag :pending
+  test "Regonizes 7" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "  |",
+        "  |",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "7"}
+  end
+
+  @tag :pending
+  test "Recognizes 8" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "|_|",
+        "|_|",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "8"}
+  end
+
+  @tag :pending
+  test "Recognizes 9" do
+    number = OCRNumbers.convert(
+      [
+        " _ ",
+        "|_|",
+        " _|",
+        "   "
+      ]
+    )
+
+    assert number == {:ok, "9"}
+  end
+
+  @tag :pending
+  test "Recognizes string of decimal numbers" do
+    number = OCRNumbers.convert(
+      [
+        "    _  _     _  _  _  _  _  _ ",
+        "  | _| _||_||_ |_   ||_||_|| |",
+        "  ||_  _|  | _||_|  ||_| _||_|",
+        "                              "
+      ]
+    )
+
+    assert number == {:ok, "1234567890"}
+  end
+
+  @tag :pending
+  test "Numbers separated by empty lines are recognized. Lines are joined by commas." do
+    number = OCRNumbers.convert(
+      [
+        "    _  _ ",
+        "  | _| _|",
+        "  ||_  _|",
+        "         ",
+        "    _  _ ",
+        "|_||_ |_ ",
+        "  | _||_|",
+        "         ",
+        " _  _  _ ",
+        "  ||_||_|",
+        "  ||_| _|",
+        "         "
+      ]
+    )
+
+    assert number == {:ok, "123,456,789"}
+  end
+end
