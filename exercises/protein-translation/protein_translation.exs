5ae5d2e937b4bcc16f78a442b155bb63f9a4ef44
Chris Doggett <doggettc@gmail.com>
2017-03-14 13:27:45 -0500

    Implement protein-translation exercise (#308)
    * Implement protein-translation exercise
    
    * Flipped test expectations, return tuple for :ok case

---
+++ exercises/protein-translation/protein_translation.exs
@@ -0,0 +1,34 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: { atom,  list(String.t()) }
+  def of_rna(rna) do
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: { atom, String.t() }
+  def of_codon(codon) do
+  end
+end
+
