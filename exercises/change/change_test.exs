b69bdc91e19fd173c6d166d8a0fd087259045988
parkerl <parker@earthvectors.com>
2017-03-25 10:38:59 -0600

    Reorder tests according to canonical spec

--- exercises/change/change_test.exs
+++ exercises/change/change_test.exs
@@ -9,25 +9,6 @@ defmodule ChangeTest do
   use ExUnit.Case
 
   # @tag :pending
-  test "no coins make 0 change" do
-    coins = [1, 5, 10, 21, 25]
-    expected = []
-    assert Change.generate(coins, 0) == {:ok, expected}
-  end
-
-  @tag :pending
-  test "cannot find negative change values" do
-    coins = [1, 2, 5]
-    assert Change.generate(coins, -5) == {:error, "cannot change"}
-  end
-
-  @tag :pending
-  test "error testing for change smaller than the smallest of coins" do
-    coins = [5, 10]
-    assert Change.generate(coins, 3) == {:error, "cannot change"}
-  end
-
-  @tag :pending
   test "single coin change" do
     coins = [1, 5, 10, 25, 100]
     expected = [25]
@@ -39,13 +20,6 @@ defmodule ChangeTest do
     coins = [1, 5, 10, 25, 100]
     expected = [5, 10]
     assert Change.generate(coins, 15) == {:ok, expected}
-  end
-
-  @tag :pending
-  test "possible change without unit coins available" do
-    coins = [2, 5, 10, 20, 50]
-    expected = [2, 2, 2, 5, 10]
-    assert Change.generate(coins, 21) == {:ok, expected}
   end
 
   @tag :pending
@@ -70,8 +44,34 @@ defmodule ChangeTest do
   end
 
   @tag :pending
+  test "possible change without unit coins available" do
+    coins = [2, 5, 10, 20, 50]
+    expected = [2, 2, 2, 5, 10]
+    assert Change.generate(coins, 21) == {:ok, expected}
+  end
+
+  @tag :pending
+  test "no coins make 0 change" do
+    coins = [1, 5, 10, 21, 25]
+    expected = []
+    assert Change.generate(coins, 0) == {:ok, expected}
+  end
+
+  @tag :pending
+  test "error testing for change smaller than the smallest of coins" do
+    coins = [5, 10]
+    assert Change.generate(coins, 3) == {:error, "cannot change"}
+  end
+
+  @tag :pending
   test "error if no combination can add up to target" do
     coins = [5, 10]
     assert Change.generate(coins, 94) == {:error, "cannot change"}
+  end
+
+  @tag :pending
+  test "cannot find negative change values" do
+    coins = [1, 2, 5]
+    assert Change.generate(coins, -5) == {:error, "cannot change"}
   end
 end
