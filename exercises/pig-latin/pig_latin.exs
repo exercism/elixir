486bb879057bd9b5bd756b3070955e55ad1586e9
Chris Doggett <doggettc@gmail.com>
2017-03-26 22:59:17 -0500

    Implement Pig Latin exercise (#315)
    * Implement Pig Latin exercise
    
    * Add describe blocks to test groups

---
+++ exercises/pig-latin/pig_latin.exs
@@ -0,0 +1,20 @@
+defmodule PigLatin do
+  @doc """
+  Given a `phrase`, translate it a word at a time to Pig Latin.
+
+  Words beginning with consonants should have the consonant moved to the end of
+  the word, followed by "ay".
+
+  Words beginning with vowels (aeiou) should have "ay" added to the end of the
+  word.
+
+  Some groups of letters are treated like consonants, including "ch", "qu",
+  "squ", "th", "thr", and "sch".
+
+  Some groups are treated like vowels, including "yt" and "xr".
+  """
+  @spec translate(phrase :: String.t()) :: String.t()
+  def translate(phrase) do
+  end
+end
+
