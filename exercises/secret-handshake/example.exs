d4f7caee0986be6518b3cc6227f9cfaeaf6b5631
Chris Doggett <CDoggett@covermymeds.com>
2017-03-29 11:33:56 -0500

    Refactored to use Bitwise, expanded HINTS.md

--- exercises/secret-handshake/example.exs
+++ exercises/secret-handshake/example.exs
@@ -1,10 +1,7 @@
 defmodule SecretHandshake do
-  @codes %{
-    1 => "wink",
-    2 => "double blink",
-    4 => "close your eyes",
-    8 => "jump"
-  }
+  use Bitwise
+
+  @codes ["wink", "double blink", "close your eyes", "jump"]
 
   @doc """
   Determine the actions of a secret handshake based on the binary
@@ -22,16 +19,25 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    do_commands(div(code, 2), rem(code, 2), 1, [])
+    @codes
+    |> Enum.with_index
+    |> Enum.map(fn {command, i} -> { command, 2 <<< (i-1) } end)
+    |> decode_commands(code, [])
   end
 
-  def do_commands(_code, _set, 32, results), do: Enum.reverse(results)
-  def do_commands(_code, 1, 16, results), do: results
-  for {mult, result} <- @codes do
-    def do_commands(code, 1, unquote(mult), results) do
-      do_commands(div(code, 2), rem(code, 2), unquote(mult) * 2, [unquote(result) | results])
+  defp decode_commands([], code, results) do
+    case flag_set(code, 16) do
+      true -> results
+      _ -> Enum.reverse(results)
     end
   end
-  def do_commands(code, 0, mult, results), do: do_commands(div(code, 2), rem(code, 2), mult * 2, results)
+  defp decode_commands([{ command, flag } | flags], code, results) do
+    case flag_set(code, flag) do
+      true -> decode_commands(flags, code, [command | results])
+      _ -> decode_commands(flags, code, results)
+    end
+  end
+
+  defp flag_set(flags, flag_to_check), do: (flags &&& flag_to_check) == flag_to_check
 end
 
