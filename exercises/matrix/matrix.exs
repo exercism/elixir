005845b64d5ff599275e74d8ed99be82deffb975
Chris Doggett <CDoggett@covermymeds.com>
2017-04-18 16:34:04 -0500

    Renamed to Matrix, cleared up test.
    Depends on https://github.com/exercism/xelixir/pull/332 renaming
    SaddlePoints' Matrix class to SaddlePoints. Until then, this will fail
    CI build.

--- exercises/matrix/matrix.exs
+++ exercises/matrix/matrix.exs
@@ -1,11 +1,11 @@
-defmodule MatrixStruct do
+defmodule Matrix do
   defstruct matrix: nil
 
   @doc """
   Convert an `input` string, with rows separated by newlines and values
-  separated by single spaces, into a `MatrixStruct` struct.
+  separated by single spaces, into a `Matrix` struct.
   """
-  @spec from_string(input :: String.t()) :: %MatrixStruct{}
+  @spec from_string(input :: String.t()) :: %Matrix{}
   def from_string(input) do
   end
 
@@ -13,35 +13,35 @@ defmodule MatrixStruct do
   Write the `matrix` out as a string, with rows separated by newlines and
   values separated by single spaces.
   """
-  @spec to_string(matrix :: %MatrixStruct{}) :: String.t()
+  @spec to_string(matrix :: %Matrix{}) :: String.t()
   def to_string(matrix) do
   end
 
   @doc """
   Given a `matrix`, return its rows as a list of lists of integers.
   """
-  @spec rows(matrix :: %MatrixStruct{}) :: list(list(integer))
+  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
   def rows(matrix) do
   end
 
   @doc """
   Given a `matrix` and `index`, return the row at `index`.
   """
-  @spec row(matrix :: %MatrixStruct{}, index :: integer) :: list(integer)
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
   def row(matrix, index) do
   end
 
   @doc """
   Given a `matrix`, return its columns as a list of lists of integers.
   """
-  @spec columns(matrix :: %MatrixStruct{}) :: list(list(integer))
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
   def columns(matrix) do
   end
 
   @doc """
   Given a `matrix` and `index`, return the column at `index`.
   """
-  @spec column(matrix :: %MatrixStruct{}, index :: integer) :: list(integer)
+  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
   def column(matrix, index) do
   end
 end
--- exercises/matrix/matrix_test.exs
+++ exercises/matrix/matrix_test.exs
@@ -12,23 +12,15 @@ defmodule MatrixTest do
 
   #@tag :pending
   test "reading from and writing to string" do
-    matrix = MatrixStruct.from_string(@input)
-
-    assert matrix != nil
-
-    %MatrixStruct{ matrix: parsed_matrix } = matrix
-
-    # You can represent the internal structure however you want
-    assert parsed_matrix != nil
-
-    assert MatrixStruct.to_string(matrix) == @input
+    matrix = Matrix.from_string(@input)
+    assert Matrix.to_string(matrix) == @input
   end
 
   @tag :pending
   test "rows should return nested lists regardless of internal structure" do
-    matrix = MatrixStruct.from_string(@input)
+    matrix = Matrix.from_string(@input)
 
-    assert MatrixStruct.rows(matrix) == [
+    assert Matrix.rows(matrix) == [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
@@ -37,18 +29,18 @@ defmodule MatrixTest do
 
   @tag :pending
   test "row should return list at index" do
-    matrix = MatrixStruct.from_string(@input)
+    matrix = Matrix.from_string(@input)
 
-    assert MatrixStruct.row(matrix, 0) == [1, 2, 3]
-    assert MatrixStruct.row(matrix, 1) == [4, 5, 6]
-    assert MatrixStruct.row(matrix, 2) == [7, 8, 9]
+    assert Matrix.row(matrix, 0) == [1, 2, 3]
+    assert Matrix.row(matrix, 1) == [4, 5, 6]
+    assert Matrix.row(matrix, 2) == [7, 8, 9]
   end
 
   @tag :pending
   test "columns should return nested lists regardless of internal structure" do
-    matrix = MatrixStruct.from_string(@input)
+    matrix = Matrix.from_string(@input)
 
-    assert MatrixStruct.columns(matrix) == [
+    assert Matrix.columns(matrix) == [
       [1, 4, 7],
       [2, 5, 8],
       [3, 6, 9]
@@ -57,10 +49,10 @@ defmodule MatrixTest do
 
   @tag :pending
   test "column should return list at index" do
-    matrix = MatrixStruct.from_string(@input)
+    matrix = Matrix.from_string(@input)
 
-    assert MatrixStruct.column(matrix, 0) == [1, 4, 7]
-    assert MatrixStruct.column(matrix, 1) == [2, 5, 8]
-    assert MatrixStruct.column(matrix, 2) == [3, 6, 9]
+    assert Matrix.column(matrix, 0) == [1, 4, 7]
+    assert Matrix.column(matrix, 1) == [2, 5, 8]
+    assert Matrix.column(matrix, 2) == [3, 6, 9]
   end
 end
