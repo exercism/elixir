72653443158bb0e06c158a35d7fd03dd94e38244
Tonći Galić <tuxified@gmail.com>
2017-06-17 13:13:56 +0200

    introduce collatz conjecture exercise
    adds Collatz Conjecture exercise: https://github.com/exercism/x-common/blob/master/exercises/collatz-conjecture/description.md

---
+++ exercises/collatz-conjecture/collatz_conjecture.exs
@@ -0,0 +1,13 @@
+defmodule CollatzConjecture do
+
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(number :: integer) :: pos_integer
+  def calc(input) do
+    raise "ain't got no time for that"
+  end
+end
---
+++ exercises/collatz-conjecture/example.exs
@@ -0,0 +1,25 @@
+defmodule CollatzConjecture do
+
+  @doc """
+  calc/1 takes number (> 1), and returns the number of steps required to get to 1 when
+  following the rules: if n is odd, multiply with 3 and add 1. if n is even, divide by 2
+  """
+  @spec calc(number :: integer) :: pos_integer
+  def calc(number) when number < 1 do
+    raise ArgumentError, "Only positive numbers are allowed"
+  end
+
+  def calc(number) do
+    calc(number, 0)
+  end
+
+  defp calc(1, steps), do: steps
+  defp calc(number, steps) when rem(number, 2) == 0 do
+    calc(div(number, 2), steps + 1)
+  end
+
+  defp calc(number, steps) do
+    calc(number * 3 + 1, steps + 1)
+  end
+end
+
