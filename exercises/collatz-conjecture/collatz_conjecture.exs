6a9ae8caed0fd78388ee7a3bc30a490275ce841e
Tonći Galić <tuxified@gmail.com>
2017-06-22 09:44:14 +0200

    introduce collatz conjecture exercise (#346)
    * introduce collatz conjecture exercise
    
    adds Collatz Conjecture exercise: https://github.com/exercism/x-common/blob/master/exercises/collatz-conjecture/description.md
    
    * update config to include newest exercise
    
    * resort to FunctinClause Errors for invalid input
    
    * mark only first test as _not_ pending

---
+++ exercises/collatz-conjecture/collatz_conjecture.exs
@@ -0,0 +1,12 @@
+defmodule CollatzConjecture do
+
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(number :: pos_integer) :: pos_integer
+  def calc(input) do
+  end
+end
