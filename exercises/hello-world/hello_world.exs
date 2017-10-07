18b26cba3e7a9059c97f48afe39882b3f7db2ca6
David McKay <david.andrew.mckay@gmail.com>
2017-10-07 12:08:37 +0100

    Adding example

--- exercises/hello-world/hello_world.exs
+++ exercises/hello-world/hello_world.exs
@@ -2,10 +2,8 @@ defmodule HelloWorld do
   @doc """
   Simply returns "Hello, world!"
   """
-  @spec hello() :: String.t
-  def hello() do
-
+  @spec hello :: String.t
+  def hello do
     "Your implementation goes here"
-
   end
 end
