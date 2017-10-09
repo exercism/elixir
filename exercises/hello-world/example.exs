4f8228c1932f80be6a0c48f4a29c3047587a4fe9
David McKay <david.andrew.mckay@gmail.com>
2017-10-09 10:43:02 +0100

    Updated hello_world problem to reflect exercism/problem-specifications#520 (#370)
    * Updated hello_world problem to reflect exercism/problem-specifications#520
    
    https://github.com/exercism/problem-specifications/issues/520
    
    * Adding example

--- exercises/hello-world/example.exs
+++ exercises/hello-world/example.exs
@@ -1,13 +1,9 @@
 defmodule HelloWorld do
-
   @doc """
   Greets the user by name, or by saying "Hello, World!"
   if no name is given
   """
-
-  def hello(name \\ "World") do
-
-    "Hello, " <> name <> "!"
-
+  def hello do
+    "Hello, World!"
   end
-end
+ end
