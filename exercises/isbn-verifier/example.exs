8e8271e93727b1a24b55c098258a0c3b99f3d91a
Stefan Osorio <stefan@osorio.de>
2017-10-27 00:42:40 +0200

    Wrote example implementation for isbn-verifier

---
+++ exercises/isbn-verifier/example.exs
@@ -0,0 +1,36 @@
+defmodule ISBNVerifier do
+  @doc """
+    Checks if a string is a valid ISBN-10 identifier
+
+    ## Examples
+
+      iex> ISBNVerifier.isbn?("3-598-21507-X")
+      true
+
+      iex> ISBNVerifier.isbn?("3-598-2K507-0")
+      false
+
+  """
+  @spec isbn?(String.t()) :: boolean
+  def isbn?(isbn) do
+    if Regex.match?(~r/^(\d-?){9}(\d|X)$/, isbn) do
+      checksum =
+        isbn
+        |> String.replace("-", "")
+        |> String.graphemes()
+        |> Enum.zip(10..1)
+        |> Enum.map(&checksum_weighting/1)
+        |> Enum.sum()
+
+      rem(checksum, 11) == 0
+    else
+      false
+    end
+  end
+
+  defp checksum_weighting({"X", 1}), do: 10
+
+  defp checksum_weighting({digit, weight}) do
+    String.to_integer(digit) * weight
+  end
+end
