3231885b36c9a19f8a5b72902a8cfbb291a13ab8
flyrry <sergeimail@gmail.com>
2017-10-31 16:57:07 +0200

    New exercise: Spiral matrix (#380)
    * add spiral-matrix readme, tests and example implementation
    
    * mark all tests but first as pending
    
    * minor style corrections
    
    * add spiral-matrix to config.json

---
+++ exercises/spiral-matrix/spiral.exs
@@ -0,0 +1,9 @@
+defmodule Spiral do
+
+  @doc """
+  Given the dimension, return a square matrix of numbers in clockwise spiral order.
+  """
+  @spec matrix(dimension :: integer) :: list(list(integer))
+  def matrix(dimension) do
+  end
+end
