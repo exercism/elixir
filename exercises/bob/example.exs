96be49294d75c4396af94f99c52c4f92a2032169
Victor Presumido <victor.presumido@gmail.com>
2018-02-27 00:00:24 -0300

    Update example with possible solution

--- exercises/bob/example.exs
+++ exercises/bob/example.exs
@@ -20,6 +20,7 @@ defmodule Bob do
   def hey(input) do
     cond do
       silent?(input) -> "Fine. Be that way!"
+      shouting_question?(input) -> "Calm down, I know what I'm doing!"
       question?(input) -> "Sure."
       shouting?(input) -> "Whoa, chill out!"
       true -> "Whatever."
@@ -29,6 +30,7 @@ defmodule Bob do
   defp silent?(input), do: "" == String.trim(input)
   defp shouting?(input), do: input == String.upcase(input) && letters?(input)
   defp question?(input), do: String.ends_with?(input, "?")
+  defp shouting_question?(input), do: shouting?(input) && question?(input)
   defp letters?(input), do: Regex.match?(~r/\p{L}+/, input)
 end
 
@@ -38,21 +40,23 @@ end
 # warning)
 
 # defmodule Message do
-#   def silent?(input),   do: "" == String.strip(input)
-#   def shouting?(input), do: input == String.upcase(input) && letters?(input)
-#   def question?(input), do: String.ends_with?(input, "?")
-#   defp letters?(input), do: Regex.match?(~r/\p{L}+/, input)
+#   def silent?(input),            do: "" == String.strip(input)
+#   def shouting_question?(input), do: shouting?(input) && question?(input)
+#   def shouting?(input),          do: input == String.upcase(input) && letters?(input)
+#   def question?(input),          do: String.ends_with?(input, "?")
+#   defp letters?(input),          do: Regex.match?(~r/\p{L}+/, input)
 # end
 #
 # defmodule Bob do
-#   import Message, only: [silent?: 1, shouting?: 1, question?: 1]
+#   import Message, only: [silent?: 1, shouting_question?: 1, shouting?: 1, question?: 1]
 #
 #   def hey(input) do
 #     cond do
-#       silent?(input)   -> "Fine. Be that way!"
-#       shouting?(input) -> "Whoa, chill out!"
-#       question?(input) -> "Sure."
-#       true             -> "Whatever."
+#       silent?(input)            -> "Fine. Be that way!"
+#       shouting_question?(input) -> "Calm down, I know what I'm doing!"
+#       shouting?(input)          -> "Whoa, chill out!"
+#       question?(input)          -> "Sure."
+#       true                      -> "Whatever."
 #     end
 #   end
 # end
