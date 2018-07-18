1fcc7d72819c04262d77487ead80186296779271
Bruno Souza Cruz <b.s.cruz951@gmail.com>
2018-07-18 19:14:15 -0300

    [armstrong_numbers] Translate exercise Armstrong Numbers, create tests, example, readme and add config (#412)

---
+++ exercises/armstrong-numbers/armstrong_number.exs
@@ -0,0 +1,9 @@
+defmodule ArmstrongNumber do
+  @moduledoc """
+  Provides a way to validate whether or not a number is an Armstrong number
+  """
+
+  @spec is_valid?(integer) :: boolean
+  def is_valid?(number) do
+  end
+end
