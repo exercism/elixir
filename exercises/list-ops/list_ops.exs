6003fad4ab45fa7b83d041939531d66a5f7a7c9d
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2018-09-21 13:02:44 -0400

    list-ops: clarify allowed functions

--- exercises/list-ops/list_ops.exs
+++ exercises/list-ops/list_ops.exs
@@ -1,10 +1,9 @@
 defmodule ListOps do
-  # Please don't use any external modules (especially List) in your
-  # implementation. The point of this exercise is to create these basic functions
-  # yourself.
-  #
-  # Note that `++` is a function from an external module (Kernel, which is
-  # automatically imported) and so shouldn't be used either.
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
   def count(l) do
