dca181096720f723f65dcd09ee0c801947e346b0
Angelika Tyborska <atyborska93@gmail.com>
2018-11-14 20:55:33 +0100

    Remove invalid spec from queen-attack
    Because of this invalid spec, the solution boilerplate wouldn't even
    compile due to:
    ```
    ** (CompileError) queen_attack.exs:8: spec for undefined function new/0
    ```

--- exercises/queen-attack/queen_attack.exs
+++ exercises/queen-attack/queen_attack.exs
@@ -5,13 +5,12 @@ defmodule Queens do
   @doc """
   Creates a new set of Queens
   """
-  @spec new() :: Queens.t()
   @spec new({integer, integer}, {integer, integer}) :: Queens.t()
   def new(white, black) do
   end
 
   @doc """
-  Gives a string reprentation of the board with
+  Gives a string representation of the board with
   white and black queen locations shown
   """
   @spec to_string(Queens.t()) :: String.t()
