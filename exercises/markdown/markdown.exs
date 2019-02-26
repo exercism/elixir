b02d702d1ecb98179286d3315029199b6388dc1b
Mathis Raguin <mathis.raguin@epita.fr>
2019-02-26 15:10:04 +0100

    remove iex prompt in exercise markdown documentation
    Signed-off-by: Mathis Raguin <mathis.raguin@epita.fr>

--- exercises/markdown/markdown.exs
+++ exercises/markdown/markdown.exs
@@ -4,10 +4,10 @@ defmodule Markdown do
 
     ## Examples
 
-    iex> Markdown.parse("This is a paragraph")
+    Markdown.parse("This is a paragraph")
     "<p>This is a paragraph</p>"
 
-    iex> Markdown.parse("# Header!\n* __Bold Item__\n* _Italic Item_")
+    Markdown.parse("# Header!\n* __Bold Item__\n* _Italic Item_")
     "<h1>Header!</h1><ul><li><strong>Bold Item</strong></li><li><em>Italic Item</em></li></ul>"
   """
   @spec parse(String.t()) :: String.t()
