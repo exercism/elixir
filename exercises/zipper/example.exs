1ec3028feeec57a2b3e57119d2be5725a766c1b7
Cohen Carlisle <Cohen-Carlisle@users.noreply.github.com>
2019-03-20 09:20:12 +0100

    Update exercises/zipper/example.exs
    Co-Authored-By: devonestes <devon.c.estes@gmail.com>

--- exercises/zipper/example.exs
+++ exercises/zipper/example.exs
@@ -18,7 +18,7 @@ defimpl Inspect, for: BinTree do
   # A custom inspect instance purely for the tests, this makes error messages
   # much more readable.
   #
-  # BinTree[value: 3, left: BinTree[value: 5, right: BinTree[value: 6]]] becomes (3:(5::(6::)):)
+  # %BinTree{value: 3, left: %BinTree{value: 5, right: %BinTree{value: 6}}} becomes (3:(5::(6::)):)
   def inspect(%BinTree{value: value, left: left, right: right}, opts) do
     concat([
       "(",
