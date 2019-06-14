6d3352d899a5a3d4e2a659ce9e45d28a3f91df9c
Tim Austin <tim@neenjaw.com>
2019-06-14 11:50:16 -0600

    start of a bash script to programmatically do this

---
+++ mixify_exercise.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# mkdir <exercise>/bkp
+# mix new <exercise>
+# cp <exercise>/bkp/<exercise>_test.exs <exercise>/test/
+# trim leading exercise stuff
+# rm <exercise>/lib/exercise.ex
+# cp <exercise>/bkp/<exercise>.exs <exercise>/lib/exercise.ex
