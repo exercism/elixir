d3f54c6277d0edb69d79e64ce099bc172fc149a4
Tim Austin <tim@neenjaw.com>
2019-06-14 12:35:03 -0600

    accumulate mixified

---
+++ exercises/accumulate/lib/accumulate.ex
@@ -0,0 +1,21 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate(list, fun) do
+  end
+end
--- mixify_exercise.sh
+++ mixify_exercise.sh
@@ -1,8 +1,11 @@
 #!bin/bash
 
+# cd exercises
 # mkdir <exercise>/bkp
+# mv <exercise>/<exercise>*.exs <exercise>/bkp
 # mix new <exercise>
 # cp <exercise>/bkp/<exercise>_test.exs <exercise>/test/
+# add configure line to <exercise>/test/test_helper.exs
 # trim leading exercise stuff
 # rm <exercise>/lib/exercise.ex
 # cp <exercise>/bkp/<exercise>.exs <exercise>/lib/exercise.ex
