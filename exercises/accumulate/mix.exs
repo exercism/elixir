a794fb68d2a8a07ca431b4c5feaa26443cde311a
Tim Austin <tim@neenjaw.com>
2019-07-04 00:11:57 -0600

    update accumulate mix.exs to include compile flag

--- exercises/accumulate/mix.exs
+++ exercises/accumulate/mix.exs
@@ -7,7 +7,14 @@ defmodule Accumulate.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  def aliases do
+    [
+      "compile": ["compile --warnings-as-errors"]
     ]
   end
 
