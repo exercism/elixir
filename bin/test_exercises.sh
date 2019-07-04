fa5101134d6d0b793349da16fbfe6e7cd5342820
Tim Austin <tim@neenjaw.com>
2019-07-04 00:01:02 -0600

    test exercises started for travisCI

---
+++ bin/test_exercises.sh
@@ -0,0 +1,14 @@
+# make copy of exercises folder
+# for each exercise
+#   create a backup of the lib
+#   move the example into lib
+#   test
+# delete test folder
+
+for file in lib/*.ex
+    do  mv "$file" "${file/.ex/.ex.bkp}"
+done
+
+mv example.exs lib/example.ex
+
+mix test --include pending
