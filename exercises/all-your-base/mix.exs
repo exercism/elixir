f6a22160ed15a79c4984263bff15fbf384b6e8a1
Tim Austin <tim@neenjaw.com>
2019-07-04 22:00:11 -0600

    deps fix

--- exercises/all-your-base/mix.exs
+++ exercises/all-your-base/mix.exs
@@ -7,7 +7,7 @@ defmodule AllYourBase.MixProject do
       version: "0.1.0",
       # elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-,
+      deps: deps(),
       aliases: aliases()
     ]
   end
