16f503898c768725f056aa60455fe12e98b5f49f
Tim Austin <tim@neenjaw.com>
2019-07-11 09:41:58 -0600

    removed mix alias

--- exercises/two-fer/mix.exs
+++ exercises/two-fer/mix.exs
@@ -7,8 +7,7 @@ defmodule TwoFer.MixProject do
       version: "0.1.0",
       # elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      aliases: aliases()
+      deps: deps()
     ]
   end
 
@@ -24,12 +23,6 @@ defmodule TwoFer.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
-  end
-
-  defp aliases do
-    [
-      compile: ["compile --warnings-as-errors"]
     ]
   end
 end
