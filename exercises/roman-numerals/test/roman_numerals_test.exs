be56e6ee758050f3eeec61a31cbabf5d395ba30c
Edgar Tinajero <cetinajero@users.noreply.github.com>
2019-08-07 02:34:56 -0600

    Rename roman-numerals function to 'numeral' for better reading (#479)
    * Rename roman.exs to roman_numerals.exs
    
    Just a little filename rename for consistency
    
    * Point roman-numerals tests to the new script
    
    * Rename roman-numerals module names for consistency
    
    * Rename roman-numerals function to 'numeral' for better reading
    
    Depends on #477
    
    * Update roman-numerals mix file to the new structure

--- exercises/roman-numerals/test/roman_numerals_test.exs
+++ exercises/roman-numerals/test/roman_numerals_test.exs
@@ -3,91 +3,91 @@ defmodule RomanNumeralsTest do
 
   # @tag :pending
   test "1" do
-    assert RomanNumerals.numerals(1) == "I"
+    assert RomanNumerals.numeral(1) == "I"
   end
 
   @tag :pending
   test "2" do
-    assert RomanNumerals.numerals(2) == "II"
+    assert RomanNumerals.numeral(2) == "II"
   end
 
   @tag :pending
   test "3" do
-    assert RomanNumerals.numerals(3) == "III"
+    assert RomanNumerals.numeral(3) == "III"
   end
 
   @tag :pending
   test "4" do
-    assert RomanNumerals.numerals(4) == "IV"
+    assert RomanNumerals.numeral(4) == "IV"
   end
 
   @tag :pending
   test "5" do
-    assert RomanNumerals.numerals(5) == "V"
+    assert RomanNumerals.numeral(5) == "V"
   end
 
   @tag :pending
   test "6" do
-    assert RomanNumerals.numerals(6) == "VI"
+    assert RomanNumerals.numeral(6) == "VI"
   end
 
   @tag :pending
   test "9" do
-    assert RomanNumerals.numerals(9) == "IX"
+    assert RomanNumerals.numeral(9) == "IX"
   end
 
   @tag :pending
   test "27" do
-    assert RomanNumerals.numerals(27) == "XXVII"
+    assert RomanNumerals.numeral(27) == "XXVII"
   end
 
   @tag :pending
   test "48" do
-    assert RomanNumerals.numerals(48) == "XLVIII"
+    assert RomanNumerals.numeral(48) == "XLVIII"
   end
 
   @tag :pending
   test "59" do
-    assert RomanNumerals.numerals(59) == "LIX"
+    assert RomanNumerals.numeral(59) == "LIX"
   end
 
   @tag :pending
   test "93" do
-    assert RomanNumerals.numerals(93) == "XCIII"
+    assert RomanNumerals.numeral(93) == "XCIII"
   end
 
   @tag :pending
   test "141" do
-    assert RomanNumerals.numerals(141) == "CXLI"
+    assert RomanNumerals.numeral(141) == "CXLI"
   end
 
   @tag :pending
   test "163" do
-    assert RomanNumerals.numerals(163) == "CLXIII"
+    assert RomanNumerals.numeral(163) == "CLXIII"
   end
 
   @tag :pending
   test "402" do
-    assert RomanNumerals.numerals(402) == "CDII"
+    assert RomanNumerals.numeral(402) == "CDII"
   end
 
   @tag :pending
   test "575" do
-    assert RomanNumerals.numerals(575) == "DLXXV"
+    assert RomanNumerals.numeral(575) == "DLXXV"
   end
 
   @tag :pending
   test "911" do
-    assert RomanNumerals.numerals(911) == "CMXI"
+    assert RomanNumerals.numeral(911) == "CMXI"
   end
 
   @tag :pending
   test "1024" do
-    assert RomanNumerals.numerals(1024) == "MXXIV"
+    assert RomanNumerals.numeral(1024) == "MXXIV"
   end
 
   @tag :pending
   test "3000" do
-    assert RomanNumerals.numerals(3000) == "MMM"
+    assert RomanNumerals.numeral(3000) == "MMM"
   end
 end
