cebab06d0893e4877389d4ff9fe74922286ca31b
Cohen Carlisle <cohencarlisle+github@pm.me>
2019-10-09 13:52:02 -0400

    format dnd-character

--- exercises/dnd-character/example.exs
+++ exercises/dnd-character/example.exs
@@ -1,23 +1,23 @@
 defmodule DndCharacter do
   @type t :: %__MODULE__{
-    strength: pos_integer(),
-    dexterity: pos_integer(),
-    constitution: pos_integer(),
-    intelligence: pos_integer(),
-    wisdom: pos_integer(),
-    charisma: pos_integer(),
-    hitpoints: pos_integer()
-  }
+          strength: pos_integer(),
+          dexterity: pos_integer(),
+          constitution: pos_integer(),
+          intelligence: pos_integer(),
+          wisdom: pos_integer(),
+          charisma: pos_integer(),
+          hitpoints: pos_integer()
+        }
 
   defstruct ~w[strength dexterity constitution intelligence wisdom charisma hitpoints]a
 
-  @spec modifier(pos_integer) :: integer()
+  @spec modifier(pos_integer()) :: integer()
   def modifier(score) do
     Integer.floor_div(score - 10, 2)
   end
 
   @spec ability :: pos_integer()
-  def ability() do
+  def ability do
     1..4
     |> Enum.map(fn _ -> Enum.random(1..6) end)
     |> Enum.sort()
@@ -26,7 +26,7 @@ defmodule DndCharacter do
   end
 
   @spec character :: t()
-  def character() do
+  def character do
     constitution = ability()
 
     %__MODULE__{
--- exercises/dnd-character/lib/dnd_character.ex
+++ exercises/dnd-character/lib/dnd_character.ex
@@ -1,25 +1,25 @@
 defmodule DndCharacter do
   @type t :: %__MODULE__{
-    strength: pos_integer(),
-    dexterity: pos_integer(),
-    constitution: pos_integer(),
-    intelligence: pos_integer(),
-    wisdom: pos_integer(),
-    charisma: pos_integer(),
-    hitpoints: pos_integer()
-  }
+          strength: pos_integer(),
+          dexterity: pos_integer(),
+          constitution: pos_integer(),
+          intelligence: pos_integer(),
+          wisdom: pos_integer(),
+          charisma: pos_integer(),
+          hitpoints: pos_integer()
+        }
 
   defstruct ~w[strength dexterity constitution intelligence wisdom charisma hitpoints]a
 
-  @spec modifier(pos_integer) :: integer()
+  @spec modifier(pos_integer()) :: integer()
   def modifier(score) do
   end
 
   @spec ability :: pos_integer()
-  def ability() do
+  def ability do
   end
 
   @spec character :: t()
-  def character() do
+  def character do
   end
 end
