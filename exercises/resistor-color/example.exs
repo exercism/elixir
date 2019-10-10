72f103217c73809b4e159dd60e21a4da800da949
bruno.souza <bruno.souza@locaweb.com.br>
2019-10-10 19:54:00 -0300

    [resistor-color] Translate Resistor Color exercise

---
+++ exercises/resistor-color/example.exs
@@ -0,0 +1,11 @@
+defmodule ResistorColor do
+  @moduledoc false
+
+  @colors ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+  @spec colors() :: List.t()
+  def colors, do: @colors
+
+  @spec code(String.t()) :: integer
+  def code(color), do: Enum.find_index(@colors, & &1 == color)
+end
---
+++ exercises/resistor-color/resistor_color.exs
@@ -0,0 +1,11 @@
+defmodule ResistorColor do
+  @moduledoc false
+
+  @spec colors() :: List.t()
+  def colors do
+  end
+
+  @spec code(String.t()) :: integer
+  def code(color) do
+  end
+end
