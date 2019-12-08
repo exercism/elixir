74b0cc847084230c96f00f38a9f5c2664ae53758
Tim Austin <tim@neenjaw.com>
2019-12-07 20:58:05 -0600

    binary-search-tree tests updated
    tests updated to the 1.0.0 canonical verstion.  Minimal changes
    required to align, used some describe blocks to capture the
    different use cases.

--- exercises/binary-search-tree/test/binary_search_tree_test.exs
+++ exercises/binary-search-tree/test/binary_search_tree_test.exs
@@ -1,50 +1,52 @@
 defmodule BinarySearchTreeTest do
   use ExUnit.Case
 
-  test "retains data" do
+  test "data is retained" do
     assert BinarySearchTree.new(4).data == 4
   end
 
-  @tag :pending
-  test "inserting lower number" do
-    root =
-      BinarySearchTree.new(4)
-      |> BinarySearchTree.insert(2)
+  describe "insert data at proper node" do
+    @tag :pending
+    test "smaller number at left node" do
+      root =
+        BinarySearchTree.new(4)
+        |> BinarySearchTree.insert(2)
 
-    assert root.data == 4
-    assert root.left.data == 2
+      assert root.data == 4
+      assert root.left.data == 2
+    end
+
+    @tag :pending
+    test "same number at left node" do
+      root =
+        BinarySearchTree.new(4)
+        |> BinarySearchTree.insert(4)
+
+      assert root.data == 4
+      assert root.left.data == 4
+    end
+
+    @tag :pending
+    test "greater number at right node" do
+      root =
+        BinarySearchTree.new(4)
+        |> BinarySearchTree.insert(5)
+
+      assert root.data == 4
+      assert root.right.data == 5
+    end
   end
 
   @tag :pending
-  test "inserting same number" do
-    root =
-      BinarySearchTree.new(4)
-      |> BinarySearchTree.insert(4)
-
-    assert root.data == 4
-    assert root.left.data == 4
-  end
-
-  @tag :pending
-  test "inserting higher number" do
-    root =
-      BinarySearchTree.new(4)
-      |> BinarySearchTree.insert(5)
-
-    assert root.data == 4
-    assert root.right.data == 5
-  end
-
-  @tag :pending
-  test "complex tree" do
+  test "can create complex tree" do
     root =
       BinarySearchTree.new(4)
       |> BinarySearchTree.insert(2)
       |> BinarySearchTree.insert(6)
       |> BinarySearchTree.insert(1)
       |> BinarySearchTree.insert(3)
-      |> BinarySearchTree.insert(7)
       |> BinarySearchTree.insert(5)
+      |> BinarySearchTree.insert(7)
 
     assert root.data == 4
     assert root.left.data == 2
@@ -55,42 +57,52 @@ defmodule BinarySearchTreeTest do
     assert root.right.right.data == 7
   end
 
-  @tag :pending
-  test "iterating one element" do
-    root = BinarySearchTree.new(4)
+  describe "can sort data" do
+    @tag :pending
+    test "can sort single number" do
+      root = BinarySearchTree.new(4)
 
-    assert [4] == BinarySearchTree.in_order(root)
-  end
+      assert [4] == BinarySearchTree.in_order(root)
+    end
 
-  @tag :pending
-  test "iterating over smaller element" do
-    root =
-      BinarySearchTree.new(4)
-      |> BinarySearchTree.insert(2)
+    @tag :pending
+    test "can sort if second number is smaller than first" do
+      root =
+        BinarySearchTree.new(4)
+        |> BinarySearchTree.insert(2)
 
-    assert [2, 4] == BinarySearchTree.in_order(root)
-  end
+      assert [2, 4] == BinarySearchTree.in_order(root)
+    end
 
-  @tag :pending
-  test "iterating over larger element" do
-    root =
-      BinarySearchTree.new(4)
-      |> BinarySearchTree.insert(5)
+    @tag :pending
+    test "can sort if second number is the same as the first" do
+      root =
+        BinarySearchTree.new(4)
+        |> BinarySearchTree.insert(4)
 
-    assert [4, 5] == BinarySearchTree.in_order(root)
-  end
+      assert [4, 4] == BinarySearchTree.in_order(root)
+    end
 
-  @tag :pending
-  test "iterating over complex tree" do
-    root =
-      BinarySearchTree.new(4)
-      |> BinarySearchTree.insert(2)
-      |> BinarySearchTree.insert(1)
-      |> BinarySearchTree.insert(3)
-      |> BinarySearchTree.insert(6)
-      |> BinarySearchTree.insert(7)
-      |> BinarySearchTree.insert(5)
+    @tag :pending
+    test "can sort if second number is greater than the first" do
+      root =
+        BinarySearchTree.new(4)
+        |> BinarySearchTree.insert(5)
 
-    assert [1, 2, 3, 4, 5, 6, 7] == BinarySearchTree.in_order(root)
+      assert [4, 5] == BinarySearchTree.in_order(root)
+    end
+
+    @tag :pending
+    test "can sort complex tree" do
+      root =
+        BinarySearchTree.new(2)
+        |> BinarySearchTree.insert(1)
+        |> BinarySearchTree.insert(3)
+        |> BinarySearchTree.insert(6)
+        |> BinarySearchTree.insert(7)
+        |> BinarySearchTree.insert(5)
+
+      assert [1, 2, 3, 5, 6, 7] == BinarySearchTree.in_order(root)
+    end
   end
 end
