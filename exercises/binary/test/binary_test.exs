509c39d97cd3a69ad57a62b07c60281a35090f91
Tim Austin <tim@neenjaw.com>
2019-12-07 20:05:46 -0600

    update .gitignore, binary tests
    .gitignore now includes the line `tmp-exercises` because if the exercise
    test script is interrupted, it leaves this folder with temporary files and
    should not be uploaded by accident
    
    binary exercise tests now conform to version 1.1.0 with some additional
    tests from the previous version.

--- exercises/binary/test/binary_test.exs
+++ exercises/binary/test/binary_test.exs
@@ -2,6 +2,11 @@ defmodule BinaryTest do
   use ExUnit.Case
 
   # @tag :pending
+  test "binary 0 is decimal 0" do
+    assert Binary.to_decimal("0") == 0
+  end
+
+  @tag :pending
   test "binary 1 is decimal 1" do
     assert Binary.to_decimal("1") == 1
   end
@@ -34,6 +39,41 @@ defmodule BinaryTest do
   @tag :pending
   test "binary 10001101000 is decimal 1128" do
     assert Binary.to_decimal("10001101000") == 1128
+  end
+
+  @tag :pending
+  test "binary ignores leading zeros" do
+    assert Binary.to_decimal("000011111") == 31
+  end
+
+  @tag :pending
+  test "2 is not a valid binary digit" do
+    assert Binary.to_decimal("2") == 0
+  end
+
+  @tag :pending
+  test "a number containing a non-binary digit is invalid" do
+    assert Binary.to_decimal("01201") == 0
+  end
+
+  @tag :pending
+  test "a number with trailing non-binary characters is invalid" do
+    assert Binary.to_decimal("10nope") == 0
+  end
+
+  @tag :pending
+  test "a number with leading non-binary characters is invalid" do
+    assert Binary.to_decimal("nope10") == 0
+  end
+
+  @tag :pending
+  test "a number with internal non-binary characters is invalid" do
+    assert Binary.to_decimal("10nope10") == 0
+  end
+
+  @tag :pending
+  test "a number and a word whitespace separated is invalid" do
+    assert Binary.to_decimal("001 nope") == 0
   end
 
   @tag :pending
