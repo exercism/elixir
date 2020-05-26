7def3c3dc350631acb9c17e493f8c9e1bd3ec30f
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-05-26 19:56:25 +0200

    Mark all but first test cases pending

--- exercises/concept/basics/test/lasagna_test.exs
+++ exercises/concept/basics/test/lasagna_test.exs
@@ -2,31 +2,32 @@ defmodule LasagnaTest do
   use ExUnit.Case
   doctest Lasagna
 
+  # @tag :pending
   test "expected minutes in oven" do
     assert Lasagna.expected_minutes_in_oven() === 40
   end
 
-  @tag :pending # Remove this line to run this test
+  @tag :pending
   test "remaining minutes in oven" do
     assert Lasagna.remaining_minutes_in_oven(25) === 15
   end
 
-  @tag :pending # Remove this line to run this test
+  @tag :pending
   test "preparation time in minutes for one layer" do
     assert Lasagna.preparation_time_in_minutes(1) === 2
   end
 
-  @tag :pending # Remove this line to run this test
+  @tag :pending
   test "preparation time in minutes for multiple layers" do
     assert Lasagna.preparation_time_in_minutes(4) === 8
   end
 
-  @tag :pending # Remove this line to run this test
+  @tag :pending
   test "total time in minutes for one layer" do
     assert Lasagna.total_time_in_minutes(1, 30) === 32
   end
 
-  @tag :pending # Remove this line to run this test
+  @tag :pending
   test "total time in minutes for multiple layers" do
     assert Lasagna.total_time_in_minutes(4, 8) === 16
   end
--- exercises/concept/multiple-clause-functions/test/guessing_game_test.exs
+++ exercises/concept/multiple-clause-functions/test/guessing_game_test.exs
@@ -1,26 +1,32 @@
 defmodule GuessingGameTest do
   use ExUnit.Case
 
+  # @tag :pending
   test "correct when the guessed number equals secret" do
     assert GuessingGame.compare(7, 7) == "Correct"
   end
 
+  @tag :pending
   test "too high when guessed number is greater than the secret" do
     assert GuessingGame.compare(9, 18) == "Too High"
   end
 
+  @tag :pending
   test "too low when guessed number is less than the secret" do
     assert GuessingGame.compare(42, 30) == "Too Low"
   end
 
+  @tag :pending
   test "so close when guess differs from 7 secret by -1" do
     assert GuessingGame.compare(64, 63) == "So close"
   end
 
+  @tag :pending
   test "so close when guess differs from 7 secret by +1" do
     assert GuessingGame.compare(52,53) == "So close"
   end
 
+  @tag :pending
   test "when no guess is supplied, ask the player to make a guess" do
     assert GuessingGame.compare(15) == "Make a guess"
   end
