8ca78c110a97907ca247c88489aa206e7707f917
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-10-27 22:23:48 +0100

    Rename concept exercises

--- exercises/concept/cond/lib/log_level.ex
+++
@@ -1,9 +0,0 @@
-defmodule LogLevel do
-  def to_label(level) do
-    raise "Please implement the to_label/1  function"
-  end
-
-  def send_alert?(level) do
-    raise "Please implement the send_alert?/1  function"
-  end
-end
