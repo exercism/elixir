e0ecdd7cab4894c43b239f7a1631cfd92c13eadd
John Warwick <jwarwick@gmail.com>
2020-10-30 17:33:40 -0400

    Moved whitespace check into check_formatting.sh. Removed action.

--- bin/check_formatting.sh
+++ bin/check_formatting.sh
@@ -4,6 +4,23 @@
 # check_formatting.sh
 # ###
 # Uses `mix format` to validate formatting of all elixir code
+# Looks for trailing whitespace in files
 # ###
 echo "Running 'mix format'"
 mix format --check-formatted
+FORMAT_EXIT_CODE="$?"
+
+echo "Checking for trailing whitespace"
+# git grep returns a 0 status if there is a match
+# so we negate the result for consistency
+! git grep --line-number ' $'
+GREP_EXIT_CODE="$?"
+
+if [ "$FORMAT_EXIT_CODE" -ne 0 -o "$GREP_EXIT_CODE" -ne 0 ]
+then
+  echo "Formatting checks failed"
+  exit 1;
+fi
+
+echo "Formatting checks passed"
+exit 0;
