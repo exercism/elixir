b3027e50cdb3618b88c03cc5abc04bf401485cba
John Warwick <jwarwick@gmail.com>
2020-10-30 18:34:05 -0400

    Github Actions Support (#545)
    * Adding Github Actions support
    
    * Removing previous CI files
    
    * Add dependency caching (dialyzer)

--- bin/dialyzer_check.sh
+++ bin/dialyzer_check.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 # currently dialyzer is timing out after 30 mins only on elixir 1.7 / otp 19
 if elixir --version | grep -q 'Elixir 1.7'; then
   echo "skipping dialyzer for Elixir 1.7"
   exit 0
 fi
+
+mkdir -p ./priv/plts
 
 mkdir -p ./tmp/src
 mkdir ./tmp/build
@@ -15,7 +19,7 @@ do
 done
 
 elixirc -o ./_build ./tmp/src/*.exs
-mix dialyzer
+mix dialyzer --no-check
 RESULT=$?
 rm -rf ./tmp
 
