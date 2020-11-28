fc0549602753014a7cd7255bb5dcdd40407ca8e6
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-11-28 16:05:18 +0100

    Add missing `@tag :pending` to a few concept exercises

--- exercises/concept/bread-and-potions/test/rpg_test.exs
+++ exercises/concept/bread-and-potions/test/rpg_test.exs
@@ -8,10 +8,12 @@ defmodule RPGTest do
   end
 
   describe "Edible" do
+    # @tag :pending
     test "is a protocol" do
       assert Edible.__protocol__(:functions) == [eat: 2]
     end
 
+    @tag :pending
     test "cannot eat a completely new item" do
       assert_raise Protocol.UndefinedError, fn ->
         Edible.eat(%NewItem{}, %Character{})
@@ -20,17 +22,20 @@ defmodule RPGTest do
   end
 
   describe "LoafOfBread" do
+    @tag :pending
     test "implements the Edible protocol" do
       {:consolidated, modules} = Edible.__protocol__(:impls)
       assert LoafOfBread in modules
     end
 
+    @tag :pending
     test "eating it increases health" do
       character = %Character{health: 50}
       {byproduct, %Character{} = character} = Edible.eat(%LoafOfBread{}, character)
       assert character.health == 55
     end
 
+    @tag :pending
     test "eating it has no byproduct" do
       character = %Character{}
       {byproduct, %Character{}} = Edible.eat(%LoafOfBread{}, character)
@@ -39,17 +44,20 @@ defmodule RPGTest do
   end
 
   describe "ManaPotion" do
+    @tag :pending
     test "implements the Edible protocol" do
       {:consolidated, modules} = Edible.__protocol__(:impls)
       assert ManaPotion in modules
     end
 
+    @tag :pending
     test "eating it increases mana" do
       character = %Character{mana: 10}
       {byproduct, %Character{} = character} = Edible.eat(%ManaPotion{strength: 6}, character)
       assert character.mana == 16
     end
 
+    @tag :pending
     test "eating it produces an empty bottle" do
       character = %Character{}
       {byproduct, %Character{}} = Edible.eat(%ManaPotion{}, character)
@@ -58,17 +66,20 @@ defmodule RPGTest do
   end
 
   describe "Poison" do
+    @tag :pending
     test "implements the Edible protocol" do
       {:consolidated, modules} = Edible.__protocol__(:impls)
       assert Poison in modules
     end
 
+    @tag :pending
     test "eating it brings health down to 0" do
       character = %Character{health: 120}
       {byproduct, %Character{} = character} = Edible.eat(%Poison{}, character)
       assert character.health == 0
     end
 
+    @tag :pending
     test "eating it produces an empty bottle" do
       character = %Character{}
       {byproduct, %Character{}} = Edible.eat(%Poison{}, character)
@@ -76,6 +87,7 @@ defmodule RPGTest do
     end
   end
 
+  @tag :pending
   test "eating many things one after another" do
     items = [
       %LoafOfBread{},
