b41bcff6d98246d2518f67b4dc55beeb2e97e409
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-01-29 18:49:53 +0100

    Ignore dialyzer warnings about missing protocol impl

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,28 @@
+[
+  {":0:unknown_function Function RPG.Edible.Atom.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.BitString.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Float.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Function.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Integer.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.List.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Map.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.PID.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Port.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Reference.__impl__/1 does not exist."},
+  {":0:unknown_function Function RPG.Edible.Tuple.__impl__/1 does not exist."}
+
+#  # {short_description}
+#  {":0:unknown_function Function :erl_types.t_is_opaque/1/1 does not exist."},
+#  # {short_description, warning_type}
+#  {":0:unknown_function Function :erl_types.t_to_string/1 does not exist.", :unknown_function},
+#  # {short_description, warning_type, line}
+#  {":0:unknown_function Function :erl_types.t_to_string/1 does not exist.", :unknown_function, 0},
+#  # {file, warning_type, line}
+#  {"lib/dialyxir/pretty_print.ex", :no_return, 100},
+#  # {file, warning_type}
+#  {"lib/dialyxir/warning_helpers.ex", :no_return},
+#  # {file}
+#  {"lib/dialyxir/warnings/app_call.ex"},
+#  # regex
+#  ~r/my_file\.ex.*my_function.*no local return/
+]
