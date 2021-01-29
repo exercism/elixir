33533cfc8db936fb70161fca0534b6f1d69d9209
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-01-29 17:51:57 +0100

    Fix dialyzer_check

--- bin/dialyzer_check.sh
+++ bin/dialyzer_check.sh
@@ -11,14 +11,21 @@ mkdir -p ./priv/plts
 mkdir -p ./tmp/src
 mkdir ./tmp/build
 
-for example_file in exercises/**/example.exs
+for example_file in exercises/concept/**/mix.exs
 do
   exercise_dir=$(dirname $example_file)
   exercise=$(basename $exercise_dir)
-  cp "$exercise_dir/example.exs" "./tmp/src/$exercise.exs"
+  cp "$exercise_dir/.meta/exemplar.ex" "./tmp/src/$exercise.ex"
 done
 
-elixirc -o ./_build ./tmp/src/*.exs
+for example_file in exercises/practice/**/mix.exs
+do
+  exercise_dir=$(dirname $example_file)
+  exercise=$(basename $exercise_dir)
+  cp "$exercise_dir/.meta/example.ex" "./tmp/src/$exercise.ex"
+done
+
+elixirc -o ./_build ./tmp/src/*.ex
 mix dialyzer --no-check
 RESULT=$?
 rm -rf ./tmp
