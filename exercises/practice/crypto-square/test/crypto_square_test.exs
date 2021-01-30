ad38b04a334167e00deefa271dd9a001a6941eb0
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-01-30 10:08:21 +0100

    Update crypto-square according to canoncical data

--- exercises/practice/crypto-square/test/crypto_square_test.exs
+++ exercises/practice/crypto-square/test/crypto_square_test.exs
@@ -2,32 +2,37 @@ defmodule CryptoSquareTest do
   use ExUnit.Case
 
   # @tag :pending
-  test "empty string" do
+  test "empty plaintext results in an empty ciphertext" do
     assert CryptoSquare.encode("") == ""
   end
 
   @tag :pending
-  test "perfect square" do
-    assert CryptoSquare.encode("abcd") == "ac bd"
+  test "lowercase" do
+    assert CryptoSquare.encode("A") == "a"
   end
 
   @tag :pending
-  test "uppercase string" do
-    assert CryptoSquare.encode("ABCD") == "ac bd"
+  test "remove spaces" do
+    assert CryptoSquare.encode("  b ") == "b"
   end
 
   @tag :pending
-  test "small imperfect square" do
-    assert CryptoSquare.encode("This is easy") == "tis hsy ie  sa "
+  test "remove punctuation" do
+    assert CryptoSquare.encode("@1,%!") == "1"
   end
 
   @tag :pending
-  test "punctuation and numbers" do
-    assert CryptoSquare.encode("1, 2, 3, Go! Go, for God's sake!") == "1gga 2ook 3fde gos  ors "
+  test "9 character plaintext results in 3 chunks of 3 characters" do
+    assert CryptoSquare.encode("This is fun!") == "tsf hiu isn"
   end
 
   @tag :pending
-  test "long string" do
+  test "8 character plaintext results in 3 chunks, the last one with a trailing space" do
+    assert CryptoSquare.encode("Chill out.") == "clu hlt io "
+  end
+
+  @tag :pending
+  test "54 character plaintext results in 7 chunks, the last two with trailing spaces" do
     msg = "If man was meant to stay on the ground, god would have given us roots."
     cipher = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
     assert CryptoSquare.encode(msg) == cipher
