c6f5661c134b1a6e786fb3dd5c0828ce2d022dfa
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-06 16:49:49 +0100

    Write solution boilerplate

--- exercises/concept/library-fees/.meta/exemplar.ex
+++ exercises/concept/library-fees/.meta/exemplar.ex
@@ -1,2 +1,41 @@
 defmodule LibraryFees do
+  def datetime_from_string(string) do
+    NaiveDateTime.from_iso8601!(string)
+  end
+
+  def before_noon?(datetime) do
+    Time.compare(NaiveDateTime.to_time(datetime), ~T[12:00:00]) == :lt
+  end
+
+  def return_datetime(checkout_datetime) do
+    days = if before_noon?(checkout_datetime), do: 28, else: 29
+    NaiveDateTime.add(checkout_datetime, days * 24 * 60 * 60, :second)
+  end
+
+  def days_late(planned_return_datetime, actual_return_datetime) do
+    diff =
+      Date.diff(
+        NaiveDateTime.to_date(actual_return_datetime),
+        NaiveDateTime.to_date(planned_return_datetime)
+      )
+
+    if diff < 0, do: 0, else: diff
+  end
+
+  def monday?(datetime) do
+    datetime
+    |> NaiveDateTime.to_date()
+    |> Date.day_of_week()
+    |> Kernel.==(1)
+  end
+
+  def calculate_late_fee(checkout, return, rate) do
+    checkout_datetime = datetime_from_string(checkout)
+    planned_return_datetime = return_datetime(checkout_datetime)
+    actual_return_datetime = datetime_from_string(return)
+
+    days_late = days_late(planned_return_datetime, actual_return_datetime)
+    rate = if monday?(actual_return_datetime), do: 0.5 * rate, else: rate
+    trunc(days_late * rate)
+  end
 end
--- exercises/concept/library-fees/lib/library_fees.ex
+++ exercises/concept/library-fees/lib/library_fees.ex
@@ -1,41 +1,25 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    NaiveDateTime.from_iso8601!(string)
+    raise "Please implement the datetime_from_string/1 function"
   end
 
   def before_noon?(datetime) do
-    Time.compare(NaiveDateTime.to_time(datetime), ~T[12:00:00]) == :lt
+    raise "Please implement the before_noon?/1 function"
   end
 
   def return_datetime(checkout_datetime) do
-    days = if before_noon?(checkout_datetime), do: 28, else: 29
-    NaiveDateTime.add(checkout_datetime, days * 24 * 60 * 60, :second)
+    raise "Please implement the return_datetime/1 function"
   end
 
   def days_late(planned_return_datetime, actual_return_datetime) do
-    diff =
-      Date.diff(
-        NaiveDateTime.to_date(actual_return_datetime),
-        NaiveDateTime.to_date(planned_return_datetime)
-      )
-
-    if diff < 0, do: 0, else: diff
+    raise "Please implement the days_late/2 function"
   end
 
   def monday?(datetime) do
-    datetime
-    |> NaiveDateTime.to_date()
-    |> Date.day_of_week()
-    |> Kernel.==(1)
+    raise "Please implement the monday?/1 function"
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    checkout_datetime = datetime_from_string(checkout)
-    planned_return_datetime = return_datetime(checkout_datetime)
-    actual_return_datetime = datetime_from_string(return)
-
-    days_late = days_late(planned_return_datetime, actual_return_datetime)
-    rate = if monday?(actual_return_datetime), do: 0.5 * rate, else: rate
-    trunc(days_late * rate)
+    raise "Please implement the calculate_late_fee/3 function"
   end
 end
