91862f7fd37f83f16b425da6ed9ca9f11fe50e85
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-17 07:52:15 +0100

    Bring `gigasecond` up to date with canonical data (#616)

--- exercises/practice/gigasecond/test/gigasecond_test.exs
+++ exercises/practice/gigasecond/test/gigasecond_test.exs
@@ -2,18 +2,28 @@ defmodule GigasecondTest do
   use ExUnit.Case
 
   # @tag :pending
-  test "from 4/25/2011" do
+  test "from 2011-04-25 00:00:00" do
     assert Gigasecond.from({{2011, 4, 25}, {0, 0, 0}}) == {{2043, 1, 1}, {1, 46, 40}}
   end
 
   @tag :pending
-  test "from 6/13/1977" do
+  test "from 1977-06-13 00:00:00" do
     assert Gigasecond.from({{1977, 6, 13}, {0, 0, 0}}) == {{2009, 2, 19}, {1, 46, 40}}
   end
 
   @tag :pending
-  test "from 7/19/1959" do
+  test "from 1959-19-07 00:00:00" do
     assert Gigasecond.from({{1959, 7, 19}, {0, 0, 0}}) == {{1991, 3, 27}, {1, 46, 40}}
+  end
+
+  @tag :pending
+  test "from 2015-01-24 22:00:00" do
+    assert Gigasecond.from({{2015, 1, 24}, {22, 0, 0}}) == {{2046, 10, 2}, {23, 46, 40}}
+  end
+
+  @tag :pending
+  test "from 2015-01-24 23:59:59" do
+    assert Gigasecond.from({{2015, 1, 24}, {23, 59, 59}}) == {{2046, 10, 3}, {1, 46, 39}}
   end
 
   @tag :pending
