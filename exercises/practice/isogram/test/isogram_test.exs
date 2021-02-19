729683e01c10f056cfe485ac343643eb7613d21c
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-19 08:19:28 +0100

    Bring `isogram` up to date with canonical data (#622)

--- exercises/practice/isogram/test/isogram_test.exs
+++ exercises/practice/isogram/test/isogram_test.exs
@@ -1,42 +1,68 @@
 defmodule IsogramTest do
   use ExUnit.Case
 
-  test "isogram lowercase" do
-    assert Isogram.isogram?("subdermatoglyphic")
+  # @tag :pending
+  test "empty string" do
+    assert Isogram.isogram?("")
   end
 
   @tag :pending
-  test "not isogram lowercase " do
+  test "isogram with only lower case characters" do
+    assert Isogram.isogram?("isogram")
+  end
+
+  @tag :pending
+  test "word with one duplicated character" do
     refute Isogram.isogram?("eleven")
   end
 
   @tag :pending
-  test "isogram uppercase" do
-    assert Isogram.isogram?("DEMONSTRABLY")
+  test "word with one duplicated character from the end of the alphabet" do
+    refute Isogram.isogram?("zzyzx")
   end
 
   @tag :pending
-  test "not isogram uppercase" do
-    refute Isogram.isogram?("ALPHABET")
+  test "longest reported english isogram" do
+    assert Isogram.isogram?("subdermatoglyphic")
   end
 
   @tag :pending
-  test "isogram with dash" do
-    assert Isogram.isogram?("hjelmqvist-gryb-zock-pfund-wax")
+  test "word with duplicated character in mixed case" do
+    refute Isogram.isogram?("Alphabet")
   end
 
   @tag :pending
-  test "not isogram with dash" do
-    refute Isogram.isogram?("twenty-five")
+  test "word with duplicated character in mixed case, lowercase first" do
+    refute Isogram.isogram?("alphAbet")
   end
 
   @tag :pending
-  test "phrase is isogram" do
-    assert Isogram.isogram?("emily jung schwartzkopf")
+  test "hypothetical isogrammic word with hyphen" do
+    assert Isogram.isogram?("thumbscrew-japingly")
   end
 
   @tag :pending
-  test "phrase is not isogram" do
-    refute Isogram.isogram?("the quick brown fox")
+  test "hypothetical word with duplicated character following hyphen" do
+    refute Isogram.isogram?("thumbscrew-jappingly")
+  end
+
+  @tag :pending
+  test "isogram with duplicated hyphen" do
+    assert Isogram.isogram?("six-year-old")
+  end
+
+  @tag :pending
+  test "made-up name that is an isogram" do
+    assert Isogram.isogram?("Emily Jung Schwartzkopf")
+  end
+
+  @tag :pending
+  test "duplicated character in the middle" do
+    refute Isogram.isogram?("accentor")
+  end
+
+  @tag :pending
+  test "same first and last characters" do
+    refute Isogram.isogram?("angola")
   end
 end
