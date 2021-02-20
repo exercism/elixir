0feb5a46a34d15709c6c8358ce4897bcce67a09c
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-20 09:18:30 +0100

    Bring `nucleotide-count` up to date with canonical data

--- exercises/practice/nucleotide-count/test/nucleotide_count_test.exs
+++ exercises/practice/nucleotide-count/test/nucleotide_count_test.exs
@@ -1,37 +1,52 @@
 defmodule NucleotideCountTest do
   use ExUnit.Case
 
-  # @tag :pending
-  test "empty dna string has no adenine" do
-    assert NucleotideCount.count('', ?A) == 0
+  describe "count" do
+    # @tag :pending
+    test "empty dna string has no adenine" do
+      assert NucleotideCount.count('', ?A) == 0
+    end
+
+    @tag :pending
+    test "one nucleotide" do
+      assert NucleotideCount.count('G', ?G) == 1
+    end
+
+    @tag :pending
+    test "repetitive cytosine gets counted" do
+      assert NucleotideCount.count('CCCCC', ?C) == 5
+    end
+
+    @tag :pending
+    test "counts only thymine" do
+      assert NucleotideCount.count('GGGGGTAACCCGG', ?T) == 1
+    end
   end
 
-  @tag :pending
-  test "repetitive cytosine gets counted" do
-    assert NucleotideCount.count('CCCCC', ?C) == 5
-  end
+  describe "histogram" do
+    @tag :pending
+    test "empty dna string has no nucleotides" do
+      expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+      assert NucleotideCount.histogram('') == expected
+    end
 
-  @tag :pending
-  test "counts only thymine" do
-    assert NucleotideCount.count('GGGGGTAACCCGG', ?T) == 1
-  end
+    @tag :pending
+    test "one nucleotide" do
+      expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 1}
+      assert NucleotideCount.histogram('G') == expected
+    end
 
-  @tag :pending
-  test "empty dna string has no nucleotides" do
-    expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
-    assert NucleotideCount.histogram('') == expected
-  end
+    @tag :pending
+    test "repetitive sequence has only guanine" do
+      expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 8}
+      assert NucleotideCount.histogram('GGGGGGGG') == expected
+    end
 
-  @tag :pending
-  test "repetitive sequence has only guanine" do
-    expected = %{?A => 0, ?T => 0, ?C => 0, ?G => 8}
-    assert NucleotideCount.histogram('GGGGGGGG') == expected
-  end
-
-  @tag :pending
-  test "counts all nucleotides" do
-    s = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
-    expected = %{?A => 20, ?T => 21, ?C => 12, ?G => 17}
-    assert NucleotideCount.histogram(s) == expected
+    @tag :pending
+    test "counts all nucleotides" do
+      s = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+      expected = %{?A => 20, ?T => 21, ?C => 12, ?G => 17}
+      assert NucleotideCount.histogram(s) == expected
+    end
   end
 end
