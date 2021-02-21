7846b31297925610966c30f82585314cab3d45f3
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-21 09:21:28 +0100

    Bring `tournament` up to date with canonical data

--- exercises/practice/tournament/test/tournament_test.exs
+++ exercises/practice/tournament/test/tournament_test.exs
@@ -2,6 +2,144 @@ defmodule TournamentTest do
   use ExUnit.Case
 
   # @tag :pending
+  test "just the header if no input" do
+    input = []
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
+  test "a win is three points, a loss is zero points" do
+    input = [
+      "Allegoric Alaskans;Blithering Badgers;win"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3
+      Blithering Badgers             |  1 |  0 |  0 |  1 |  0
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
+  test "a win can also be expressed as a loss" do
+    input = [
+      "Blithering Badgers;Allegoric Alaskans;loss"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3
+      Blithering Badgers             |  1 |  0 |  0 |  1 |  0
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
+  test "a different team can win" do
+    input = [
+      "Blithering Badgers;Allegoric Alaskans;win"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Blithering Badgers             |  1 |  1 |  0 |  0 |  3
+      Allegoric Alaskans             |  1 |  0 |  0 |  1 |  0
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+
+  @tag :pending
+  test "a draw is one point each" do
+    input = [
+      "Allegoric Alaskans;Blithering Badgers;draw"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Allegoric Alaskans             |  1 |  0 |  1 |  0 |  1
+      Blithering Badgers             |  1 |  0 |  1 |  0 |  1
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
+  test "There can be more than one match" do
+    input = [
+      "Allegoric Alaskans;Blithering Badgers;win",
+      "Allegoric Alaskans;Blithering Badgers;win"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6
+      Blithering Badgers             |  2 |  0 |  0 |  2 |  0
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
+  test "There can be more than one winner" do
+    input = [
+      "Allegoric Alaskans;Blithering Badgers;loss",
+      "Allegoric Alaskans;Blithering Badgers;win"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Allegoric Alaskans             |  2 |  1 |  0 |  1 |  3
+      Blithering Badgers             |  2 |  1 |  0 |  1 |  3
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
+  test "There can be more than two teams" do
+    input = [
+      "Allegoric Alaskans;Blithering Badgers;win",
+      "Blithering Badgers;Courageous Californians;win",
+      "Courageous Californians;Allegoric Alaskans;loss"
+    ]
+
+    expected =
+      """
+      Team                           | MP |  W |  D |  L |  P
+      Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6
+      Blithering Badgers             |  2 |  1 |  0 |  1 |  3
+      Courageous Californians        |  2 |  0 |  0 |  2 |  0
+      """
+      |> String.trim()
+
+    assert Tournament.tally(input) == expected
+  end
+
+  @tag :pending
   test "typical input" do
     input = [
       "Allegoric Alaskans;Blithering Badgers;win",
