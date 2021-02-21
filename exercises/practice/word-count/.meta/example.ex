bbf0e4bf97e1b7a2794b08bd0e795e04dc016783
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-21 10:24:08 +0100

    Bring `word-count` up to date with canonical data

--- exercises/practice/word-count/.meta/example.ex
+++ exercises/practice/word-count/.meta/example.ex
@@ -1,23 +1,42 @@
 defmodule WordCount do
-  @ascii_punctuation ~r/!|"|\#|\$|%|&|'|\(|\)|\*|\+|,|\.|\/|:|;|<|=|>|\?|@|\[|\\|]|\^|_|`|\{|\||}|~/
+  @doc """
+  Count the number of words in the sentence.
 
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
   def count(sentence) do
     sentence
+    |> normalize
+    |> split
+    |> count_words
+  end
+
+  defp normalize(string) do
+    separating_punctuation = ~r/[,_]/u
+    redundant_punctuation = ~r/[^\w\s-_']/u
+    apostrophes_around_words = ~r/(?<=\W)'|'(?=[\W])/u
+    apostrophes_at_the_beginning = ~r/^'|'(?=[\W])/u
+    apostrophes_at_the_end = ~r/(?<=\W)'|'$/u
+
+    string
     |> String.downcase()
-    |> remove_punctuation
-    |> to_words
-    |> summarize
+    |> String.replace(separating_punctuation, " ")
+    |> String.replace(redundant_punctuation, "")
+    |> String.replace(apostrophes_around_words, "")
+    |> String.replace(apostrophes_at_the_beginning, "")
+    |> String.replace(apostrophes_at_the_end, "")
   end
 
-  defp remove_punctuation(string), do: String.replace(string, @ascii_punctuation, " ")
-
-  defp to_words(sentence), do: List.flatten(String.split(sentence))
-
-  defp summarize(words) do
-    Enum.reduce(words, %{}, &add_count/2)
+  defp split(string) do
+    string |> String.split(~r/[\s]+/, trim: true)
   end
 
-  defp add_count(word, counts) do
-    Map.update(counts, word, 1, &(&1 + 1))
+  defp count_words(strings) do
+    get_count = fn word, count ->
+      Map.update(count, word, 1, &(&1 + 1))
+    end
+
+    strings |> Enum.reduce(%{}, get_count)
   end
 end
