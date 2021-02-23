9cf7f54790e4b08c7a20bccf23fe947e202ff75e
Luca <lcampobasso@gmail.com>
2021-02-23 22:54:00 +0100

    moved the solution to .meta

--- exercises/practice/palindrome-products/lib/palindrome_products.ex
+++ exercises/practice/palindrome-products/lib/palindrome_products.ex
@@ -3,25 +3,6 @@ defmodule PalindromeProducts do
   Generates all palindrome products from an optionally given min factor (or 1) to a given max factor.
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map
-  def generate(max_factor, min_factor \\ 1)
-  def generate(max_factor, min_factor) when min_factor > max_factor, do: raise ArgumentError
-  def generate(max_factor, min_factor) do
-    palindromes(max_factor, min_factor)
-    |> Enum.reverse
-    |> Enum.group_by(fn([f1, f2]) -> f1 * f2 end)
+  def generate(max_factor, min_factor \\ 1) do
   end
-
-  defp palindromes(max_factor, min_factor) do
-    for f1 <- min_factor..max_factor,
-        f2 <- f1..max_factor,
-        palindrome?(f1 * f2),
-        do: [f1, f2]
-  end
-
-  defp palindrome?(n) do
-    list = Integer.digits(n)
-    list == Enum.reverse(list)
-  end
-
-
 end
