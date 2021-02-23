b7dd5a79d8a0e724d9f25f74926fe87b4f5f26d8
Luca <lcampobasso@gmail.com>
2021-02-23 20:05:17 +0100

    substituted solution and tests passing

--- exercises/practice/markdown/lib/markdown.ex
+++ exercises/practice/markdown/lib/markdown.ex
@@ -12,68 +12,38 @@ defmodule Markdown do
   """
   @spec parse(String.t()) :: String.t()
   def parse(m) do
-    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+    m
+    |> String.split("\n")
+    |> Enum.map_join("", &process/1)
+    |> patch()
   end
 
-  defp process(t) do
-    if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
-    end
+  defp process(t = "#" <> _) do
+    [h, tail] = String.split(t, " ", parts: 2)
+    wrap_in_html_tag(tail, "h#{byte_size(h)}")
   end
 
-  defp parse_header_md_level(hwt) do
-    [h | t] = String.split(hwt)
-    {to_string(String.length(h)), Enum.join(t, " ")}
+  defp process("*" <> t) do
+    t
+    |> String.trim()
+    |> wrap_in_html_tag("li")
   end
 
-  defp parse_list_md_level(l) do
-    t = String.split(String.trim_leading(l, "* "))
-    "<li>" <> join_words_with_tags(t) <> "</li>"
+  defp process(t), do: wrap_in_html_tag(t, "p")
+
+  defp wrap_in_html_tag(s, tag) do
+    "<#{tag}>#{replace_md_tags(s)}</#{tag}>"
   end
 
-  defp enclose_with_header_tag({hl, htl}) do
-    "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
-  end
-
-  defp enclose_with_paragraph_tag(t) do
-    "<p>#{join_words_with_tags(t)}</p>"
-  end
-
-  defp join_words_with_tags(t) do
-    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
-  end
-
-  defp replace_md_with_tag(w) do
-    replace_suffix_md(replace_prefix_md(w))
-  end
-
-  defp replace_prefix_md(w) do
-    cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
-    end
-  end
-
-  defp replace_suffix_md(w) do
-    cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
-    end
+  defp replace_md_tags(line) do
+    line
+    |> String.replace(~r/__(.+)__/U, "<strong>\\1</strong>")
+    |> String.replace(~r/_(.+)_/U, "<em>\\1</em>")
   end
 
   defp patch(l) do
-    String.replace_suffix(
-      String.replace(l, "<li>", "<ul>" <> "<li>", global: false),
-      "</li>",
-      "</li>" <> "</ul>"
-    )
+    l
+    |> String.replace("<li>", "<ul><li>", global: false)
+    |> String.replace(~r/(<\/li>)(?!.*\1)/,"</li></ul>")
   end
 end
