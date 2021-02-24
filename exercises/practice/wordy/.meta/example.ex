271d23089566c02466d4368fd9bbf8be22a81595
Frerich Raabe <raabe@froglogic.com>
2021-02-24 10:00:47 +0100

    Added example solution to 'wordy' exercise

--- exercises/practice/wordy/.meta/example.ex
+++ exercises/practice/wordy/.meta/example.ex
@@ -1,7 +1,7 @@
 defmodule Wordy do
-  @operands [Wordy.Number]
-  @operations [Wordy.Addition, Wordy.Subtraction, Wordy.Multiplication, Wordy.Division]
-  @parts @operands ++ @operations
+  import String, only: [to_integer: 1]
+
+  @type token() :: String.t() | integer()
 
   @doc """
   Calculate the math problem in the sentence.
@@ -10,125 +10,26 @@ defmodule Wordy do
   def answer(question) do
     question
     |> tokenize
-    |> parse
-    |> validate
-    |> calculate
+    |> eval
   end
 
-  defp tokenize(sentence) do
-    Regex.scan(~r/(#{all_matchers()})/, sentence)
-    |> Enum.map(&Enum.at(&1, 0))
+  @spec tokenize(String.t()) :: list(token())
+  defp tokenize(question) do
+    question
+    |> String.trim_trailing("?")
+    |> String.split()
   end
 
-  defp all_matchers do
-    Enum.map(@parts, & &1.matcher) |> Enum.join("|")
-  end
-
-  defp parse(tokens) do
-    for token <- tokens, part = Enum.find(@parts, & &1.match?(token)) do
-      part.parse(token)
-    end
-  end
-
-  defp validate(parsed) do
-    if Enum.find(parsed, &is_function(&1)) do
-      parsed
-    else
-      raise ArgumentError
-    end
-  end
-
-  defp calculate([left | []]) do
-    left
-  end
-
-  defp calculate([left, operation, right | t]) do
-    calculate([operation.(left, right) | t])
-  end
-
-  defmodule Number do
-    def matcher do
-      "-?\\d+"
-    end
-
-    def match?(token) do
-      String.match?(token, ~r/#{matcher()}/)
-    end
-
-    def parse(token) do
-      String.to_integer(token)
-    end
-  end
-
-  defmodule Addition do
-    def matcher do
-      "plus"
-    end
-
-    def match?(token) do
-      String.match?(token, ~r/#{matcher()}/)
-    end
-
-    def parse(_) do
-      &calculate/2
-    end
-
-    def calculate(left, right) do
-      left + right
-    end
-  end
-
-  defmodule Subtraction do
-    def matcher do
-      "minus"
-    end
-
-    def match?(token) do
-      String.match?(token, ~r/#{matcher()}/)
-    end
-
-    def parse(_) do
-      &calculate/2
-    end
-
-    def calculate(left, right) do
-      left - right
-    end
-  end
-
-  defmodule Multiplication do
-    def matcher do
-      "multiplied by"
-    end
-
-    def match?(token) do
-      String.match?(token, ~r/#{matcher()}/)
-    end
-
-    def parse(_) do
-      &calculate/2
-    end
-
-    def calculate(left, right) do
-      left * right
-    end
-  end
-
-  defmodule Division do
-    def matcher do
-      "divided by"
-    end
-
-    def match?(token) do
-      String.match?(token, ~r/#{matcher()}/)
-    end
-
-    def parse(_) do
-      &calculate/2
-    end
-
-    def calculate(left, right) do
-      round(left / right)
+  @spec eval(list(token()), integer) :: integer
+  defp eval(tokens, acc \\ 0) do
+    case tokens do
+      [] -> acc
+      ["What", "is", n | rest] -> rest |> eval(to_integer(n))
+      ["plus", n | rest] -> rest |> eval(acc + to_integer(n))
+      ["minus", n | rest] -> rest |> eval(acc - to_integer(n))
+      ["multiplied", "by", n | rest] -> rest |> eval(acc * to_integer(n))
+      ["divided", "by", n | rest] -> rest |> eval(div(acc, to_integer(n)))
+      _ -> raise(ArgumentError)
     end
   end
 end
