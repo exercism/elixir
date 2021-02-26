9c4693dce70fb69d6d2fdda57253b2ba00e52758
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-26 08:53:22 +0100

    Bring `simple-cipher` up to date with canonical data

--- exercises/practice/simple-cipher/mix.exs
+++ exercises/practice/simple-cipher/mix.exs
@@ -14,7 +14,7 @@ defmodule SimpleCipher.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
