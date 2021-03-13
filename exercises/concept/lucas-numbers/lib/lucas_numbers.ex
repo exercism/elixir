61529e95b4bc0aa352d8766ee9e471f381edc5fe
Tim Austin <tim@neenjaw.com>
2021-03-13 10:46:20 -0600

    update starting module

--- exercises/concept/lucas-numbers/lib/lucas_numbers.ex
+++ exercises/concept/lucas-numbers/lib/lucas_numbers.ex
@@ -5,26 +5,7 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
-  def generate(count) when count < 1 do
-    raise ArgumentError, "must generate at least 1 number"
-  end
-
-  def generate(1) do
-    [2]
-  end
-
-  def generate(2) do
-    [2, 1]
-  end
-
   def generate(count) do
-    sequence =
-      {2, 1}
-      |> Stream.iterate(fn {a, b} -> {b, a + b} end)
-      |> Stream.map(&elem(&1, 1))
-      |> Stream.take(count - 1)
-      |> Enum.to_list()
-
-    [2 | sequence]
+    raise "Please implement the generate/1 function"
   end
 end
