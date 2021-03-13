fa24eda17a2b90f695cc999a5e84f00678c204f6
Tim Austin <tim@neenjaw.com>
2021-03-13 14:26:22 -0600

    minor changes

--- exercises/concept/lucas-numbers/test/lucas_numbers_test.exs
+++ exercises/concept/lucas-numbers/test/lucas_numbers_test.exs
@@ -5,54 +5,64 @@ defmodule LucasNumbersTest do
     assert LucasNumbers.generate(1) == [2]
   end
 
+  @tag :pending
   test "generates a sequence of length 2" do
     assert LucasNumbers.generate(2) == [2, 1]
   end
 
+  @tag :pending
   test "generates a sequence of length 3" do
     assert LucasNumbers.generate(3) == [2, 1, 3]
   end
 
+  @tag :pending
   test "generates a sequence of length 4" do
     assert LucasNumbers.generate(4) == [2, 1, 3, 4]
   end
 
+  @tag :pending
   test "generates a sequence of length 5" do
     sequence = [2, 1, 3, 4, 7]
 
     assert LucasNumbers.generate(5) == sequence
   end
 
+  @tag :pending
   test "generates a sequence of length 6" do
     sequence = [2, 1, 3, 4, 7, 11]
 
     assert LucasNumbers.generate(6) == sequence
   end
 
+  @tag :pending
   test "generates a sequence of length 7" do
     sequence = [2, 1, 3, 4, 7, 11, 18]
 
     assert LucasNumbers.generate(7) == sequence
   end
 
+  @tag :pending
   test "generates a sequence of length 8" do
     sequence = [2, 1, 3, 4, 7, 11, 18, 29]
 
     assert LucasNumbers.generate(8) == sequence
   end
 
+  @tag :pending
   test "generates a sequence of length 9" do
     sequence = [2, 1, 3, 4, 7, 11, 18, 29, 47]
 
     assert LucasNumbers.generate(9) == sequence
   end
 
+  @tag :pending
   test "generates a sequence of length 10" do
     sequence = [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
 
     assert LucasNumbers.generate(10) == sequence
   end
 
+  @tag :pending
   test "generates a sequence of length 25" do
     sequence = [
       2,
@@ -85,12 +95,14 @@ defmodule LucasNumbersTest do
     assert LucasNumbers.generate(25) == sequence
   end
 
+  @tag :pending
   test "catch incorrect non-integer arguments" do
     assert_raise ArgumentError, "count must be specified as an integer >= 1", fn ->
       LucasNumbers.generate("Hello world!")
     end
   end
 
+  @tag :pending
   test "catch incorrect integer arguments" do
     assert_raise ArgumentError, "count must be specified as an integer >= 1", fn ->
       LucasNumbers.generate(-1)
--- exercises/concept/lucas-numbers/test/test_helper.exs
+++ exercises/concept/lucas-numbers/test/test_helper.exs
@@ -1,1 +1,2 @@
 ExUnit.start()
+ExUnit.configure(exclude: :pending, trace: true)
