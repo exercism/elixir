8ab7e0fd0c0847cef838cfe9f219bd968147713b
Herminio Torres <herminiocesar@gmail.com>
2021-03-13 23:49:35 -0300

    fix spec
    The square method waits for a tuple with an atom and an integer or a tuple with an atom and a string on the test.
    And for the total method waits for a tuple with an atom and an integer.

--- exercises/practice/grains/lib/grains.ex
+++ exercises/practice/grains/lib/grains.ex
@@ -2,14 +2,14 @@ defmodule Grains do
   @doc """
   Calculate two to the power of the input minus one.
   """
-  @spec square(pos_integer) :: pos_integer
+  @spec square(pos_integer) :: {atom, pos_integer | String.t()}
   def square(number) do
   end
 
   @doc """
   Adds square of each number from 1 to 64.
   """
-  @spec total :: pos_integer
+  @spec total :: {atom, pos_integer}
   def total do
   end
 end
