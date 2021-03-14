41f60ae7ec227f1210020c2884b0b936a7dffd1f
Herminio Torres <herminiocesar@gmail.com>
2021-03-14 12:35:16 -0300

    add parentheses

--- exercises/practice/grains/lib/grains.ex
+++ exercises/practice/grains/lib/grains.ex
@@ -2,14 +2,14 @@ defmodule Grains do
   @doc """
   Calculate two to the power of the input minus one.
   """
-  @spec square(pos_integer) :: {:ok, pos_integer} | {:error, String.t()}
+  @spec square(pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
   def square(number) do
   end
 
   @doc """
   Adds square of each number from 1 to 64.
   """
-  @spec total :: {atom, pos_integer}
+  @spec total :: {:ok, pos_integer()}
   def total do
   end
 end
