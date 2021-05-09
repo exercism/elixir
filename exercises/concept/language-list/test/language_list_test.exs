ef08d2a2ac1cc82c761d18880c3037ecd5430499
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-05-09 14:32:09 +0200

    language-list

--- exercises/concept/language-list/test/language_list_test.exs
+++ exercises/concept/language-list/test/language_list_test.exs
@@ -1,104 +1,126 @@
 defmodule LanguageListTest do
   use ExUnit.Case
 
-  test "new list" do
-    assert LanguageList.new() == []
+  describe "new/0" do
+    @task_id 1
+    test "new list" do
+      assert LanguageList.new() == []
+    end
   end
 
-  test "count an empty list" do
-    assert LanguageList.new() |> LanguageList.count() == 0
+  describe "add/1" do
+    @task_id 2
+    test "add a language to a list" do
+      language = "Elixir"
+      list = [language]
+
+      assert LanguageList.new() |> LanguageList.add(language) == list
+    end
+
+    @task_id 2
+    test "add several languages to a list" do
+      list =
+        LanguageList.new()
+        |> LanguageList.add("Clojure")
+        |> LanguageList.add("Haskell")
+        |> LanguageList.add("Erlang")
+        |> LanguageList.add("F#")
+        |> LanguageList.add("Elixir")
+
+      assert list == ["Elixir", "F#", "Erlang", "Haskell", "Clojure"]
+    end
   end
 
-  test "add a language to a list" do
-    language = "Elixir"
-    list = [language]
+  describe "remove/0" do
+    @task_id 3
+    test "remove on an empty list results in error" do
+      assert_raise ArgumentError, fn -> LanguageList.new() |> LanguageList.remove() end
+    end
 
-    assert LanguageList.new() |> LanguageList.add(language) == list
+    @task_id 3
+    test "add then remove results in empty list" do
+      list =
+        LanguageList.new()
+        |> LanguageList.add("Elixir")
+        |> LanguageList.remove()
+
+      assert list == []
+    end
+
+    @task_id 3
+    test "adding two languages, when removed, removes first item" do
+      list =
+        LanguageList.new()
+        |> LanguageList.add("F#")
+        |> LanguageList.add("Elixir")
+        |> LanguageList.remove()
+
+      assert list == ["F#"]
+    end
   end
 
-  test "add several languages to a list" do
-    list =
-      LanguageList.new()
-      |> LanguageList.add("Clojure")
-      |> LanguageList.add("Haskell")
-      |> LanguageList.add("Erlang")
-      |> LanguageList.add("F#")
-      |> LanguageList.add("Elixir")
+  describe "first/0" do
+    @task_id 4
+    test "first on an empty list raises an error" do
+      assert_raise ArgumentError, fn -> LanguageList.new() |> LanguageList.first() end
+    end
 
-    assert list == ["Elixir", "F#", "Erlang", "Haskell", "Clojure"]
+    @task_id 4
+    test "add one language, then get the first" do
+      assert LanguageList.new() |> LanguageList.add("Elixir") |> LanguageList.first() == "Elixir"
+    end
+
+    @task_id 4
+    test "add a few languages, then get the first" do
+      first =
+        LanguageList.new()
+        |> LanguageList.add("Elixir")
+        |> LanguageList.add("Prolog")
+        |> LanguageList.add("F#")
+        |> LanguageList.first()
+
+      assert first == "F#"
+    end
   end
 
-  test "remove on an empty list results in error" do
-    assert_raise ArgumentError, fn -> LanguageList.new() |> LanguageList.remove() end
+  describe "count/0" do
+    @task_id 5
+    test "the count of a new list is 0" do
+      assert LanguageList.new() |> LanguageList.count() == 0
+    end
+
+    @task_id 5
+    test "the count of a one-language list is 1" do
+      count =
+        LanguageList.new()
+        |> LanguageList.add("Elixir")
+        |> LanguageList.count()
+
+      assert count == 1
+    end
+
+    @task_id 5
+    test "the count of a multiple-item list is equal to its length" do
+      count =
+        LanguageList.new()
+        |> LanguageList.add("Elixir")
+        |> LanguageList.add("Prolog")
+        |> LanguageList.add("F#")
+        |> LanguageList.count()
+
+      assert count == 3
+    end
   end
 
-  test "add then remove results in empty list" do
-    list =
-      LanguageList.new()
-      |> LanguageList.add("Elixir")
-      |> LanguageList.remove()
+  describe "exciting_list?/1" do
+    @task_id 6
+    test "an exciting language list" do
+      assert LanguageList.exciting_list?(["Clojure", "Haskell", "Erlang", "F#", "Elixir"])
+    end
 
-    assert list == []
-  end
-
-  test "adding two languages, when removed, removes first item" do
-    list =
-      LanguageList.new()
-      |> LanguageList.add("F#")
-      |> LanguageList.add("Elixir")
-      |> LanguageList.remove()
-
-    assert list == ["F#"]
-  end
-
-  test "first on an empty list raises an error" do
-    assert_raise ArgumentError, fn -> LanguageList.new() |> LanguageList.first() end
-  end
-
-  test "add one language, then get the first" do
-    assert LanguageList.new() |> LanguageList.add("Elixir") |> LanguageList.first() == "Elixir"
-  end
-
-  test "add a few languages, then get the first" do
-    first =
-      LanguageList.new()
-      |> LanguageList.add("Elixir")
-      |> LanguageList.add("Prolog")
-      |> LanguageList.add("F#")
-      |> LanguageList.first()
-
-    assert first == "F#"
-  end
-
-  test "the count of a new list is 0" do
-    assert LanguageList.new() |> LanguageList.count() == 0
-  end
-
-  test "the count of a one-language list is 1" do
-    count =
-      LanguageList.new()
-      |> LanguageList.add("Elixir")
-      |> LanguageList.count()
-
-    assert count == 1
-  end
-
-  test "the count of a multiple-item list is equal to its length" do
-    count =
-      LanguageList.new()
-      |> LanguageList.add("Elixir")
-      |> LanguageList.add("Prolog")
-      |> LanguageList.add("F#")
-      |> LanguageList.count()
-
-    assert count == 3
-  end
-
-  test "an exciting language list" do
-    assert LanguageList.exciting_list?(["Clojure", "Haskell", "Erlang", "F#", "Elixir"])
-  end
-
-  test "not an exciting language list" do
-    refute LanguageList.exciting_list?(["Java", "C", "JavaScript"])
+    @task_id 6
+    test "not an exciting language list" do
+      refute LanguageList.exciting_list?(["Java", "C", "JavaScript"])
+    end
   end
 end
