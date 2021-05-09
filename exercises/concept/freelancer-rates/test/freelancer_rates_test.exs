0a4d0934f580e72b02cfedc08101346584b40754
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-05-09 14:20:35 +0200

    Annotate tests with @task_id

--- exercises/concept/freelancer-rates/test/freelancer_rates_test.exs
+++ exercises/concept/freelancer-rates/test/freelancer_rates_test.exs
@@ -2,42 +2,51 @@ defmodule FreelancerRatesTest do
   use ExUnit.Case
 
   describe "daily_rate/1" do
+    @task_id 1
     test "it's the hourly_rate times 8" do
       assert FreelancerRates.daily_rate(50) == 400.0
     end
 
+    @task_id 1
     test "it always returns a float" do
       assert FreelancerRates.daily_rate(60) === 480.0
     end
 
+    @task_id 1
     test "it does not round" do
       assert FreelancerRates.daily_rate(55.1) == 440.8
     end
   end
 
   describe "apply_discount" do
+    @task_id 2
     test "a discount of 10% leaves 90% of the original price" do
       assert FreelancerRates.apply_discount(140.0, 10) == 126.0
     end
 
+    @task_id 2
     test "it always returns a float" do
       assert FreelancerRates.apply_discount(100, 10) == 90.0
     end
 
+    @task_id 2
     test "it doesn't round" do
       assert FreelancerRates.apply_discount(111.11, 13.5) == 96.11015
     end
   end
 
   describe "monthly_rate/1" do
+    @task_id 3
     test "it's the daily_rate times 22" do
       assert FreelancerRates.monthly_rate(62, 0.0) == 10_912
     end
 
+    @task_id 3
     test "it always returns an integer" do
       assert FreelancerRates.monthly_rate(70, 0.0) === 12_320
     end
 
+    @task_id 3
     test "the result is rounded up" do
       # 11_052.8
       assert FreelancerRates.monthly_rate(62.8, 0.0) == 11_053
@@ -45,6 +54,7 @@ defmodule FreelancerRatesTest do
       assert FreelancerRates.monthly_rate(65.2, 0.0) == 11_476
     end
 
+    @task_id 3
     test "gives a discount" do
       # 11_792 - 12% * 11_792 = 10_376.96
       assert FreelancerRates.monthly_rate(67, 12.0) == 10_377
@@ -52,14 +62,17 @@ defmodule FreelancerRatesTest do
   end
 
   describe "days_in_budget/3" do
+    @task_id 4
     test "it's the budget divided by the daily rate" do
       assert FreelancerRates.days_in_budget(1_600, 50, 0.0) == 4
     end
 
+    @task_id 4
     test "it always returns a float" do
       assert FreelancerRates.days_in_budget(520, 65, 0.0) === 1.0
     end
 
+    @task_id 4
     test "it rounds down to one decimal place" do
       # 10.02273
       assert FreelancerRates.days_in_budget(4_410, 55, 0.0) == 10.0
@@ -67,6 +80,7 @@ defmodule FreelancerRatesTest do
       assert FreelancerRates.days_in_budget(4_480, 55, 0.0) == 10.1
     end
 
+    @task_id 4
     test "it applies the discount" do
       # 1.25
       assert FreelancerRates.days_in_budget(480, 60, 20) == 1.2
