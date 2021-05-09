01b7e5c3d1016b8c6779438e93c3857a5087d5db
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-05-09 15:13:50 +0200

    name-badge

--- exercises/concept/name-badge/test/name_badge_test.exs
+++ exercises/concept/name-badge/test/name_badge_test.exs
@@ -3,24 +3,29 @@ defmodule NameBadgeTest do
   doctest NameBadge
 
   describe "print/3" do
+    @task_id 1
     test "prints the employee badge with full data" do
       assert NameBadge.print(455, "Mary M. Brown", "MARKETING") ==
                "[455] - Mary M. Brown - MARKETING"
     end
 
+    @task_id 1
     test "uppercases the department" do
       assert NameBadge.print(89, "Jack McGregor", "Procurement") ==
                "[89] - Jack McGregor - PROCUREMENT"
     end
 
+    @task_id 2
     test "prints the employee badge without id" do
       assert NameBadge.print(nil, "Barbara White", "Security") == "Barbara White - SECURITY"
     end
 
+    @task_id 3
     test "prints the owner badge" do
       assert NameBadge.print(1, "Anna Johnson", nil) == "[1] - Anna Johnson - OWNER"
     end
 
+    @task_id 3
     test "prints the owner badge without id" do
       assert NameBadge.print(nil, "Stephen Dann", nil) == "Stephen Dann - OWNER"
     end
