13f1b9c0e2042e7e34e75273c597e78e249adb33
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-05-09 13:51:12 +0200

    captains-log

--- exercises/concept/captains-log/test/captains_log_test.exs
+++ exercises/concept/captains-log/test/captains_log_test.exs
@@ -2,6 +2,7 @@ defmodule CaptainsLogTest do
   use ExUnit.Case
 
   describe "random_planet_class" do
+    @task_id 1
     test "it always returns one of the letters: D, H, J, K, L, M, N, R, T, Y" do
       planetary_classes = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 
@@ -10,6 +11,7 @@ defmodule CaptainsLogTest do
       end)
     end
 
+    @task_id 1
     test "it will eventually return each of the letters at least once" do
       planetary_classes = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 
@@ -27,10 +29,12 @@ defmodule CaptainsLogTest do
   end
 
   describe "random_ship_registry_number" do
+    @task_id 2
     test "start with \"NCC-\"" do
       assert String.starts_with?(CaptainsLog.random_ship_registry_number(), "NCC-")
     end
 
+    @task_id 2
     test "ends with a random integer between 1000 and 9999" do
       Enum.each(0..100, fn _ ->
         random_ship_registry_number = CaptainsLog.random_ship_registry_number()
@@ -49,22 +53,26 @@ defmodule CaptainsLogTest do
   end
 
   describe "random_stardate" do
+    @task_id 3
     test "is a float" do
       assert is_float(CaptainsLog.random_stardate())
     end
 
+    @task_id 3
     test "is equal to or greater than 41_000.0" do
       Enum.each(0..100, fn _ ->
         assert CaptainsLog.random_stardate() >= 41_000.0
       end)
     end
 
+    @task_id 3
     test "is less than 42_000.0" do
       Enum.each(0..100, fn _ ->
         assert CaptainsLog.random_stardate() < 42_000.0
       end)
     end
 
+    @task_id 3
     test "consecutive calls return floats with different fractional parts" do
       decimal_parts =
         Enum.map(0..10, fn _ ->
@@ -75,6 +83,7 @@ defmodule CaptainsLogTest do
       assert Enum.count(Enum.uniq(decimal_parts)) > 3
     end
 
+    @task_id 3
     test "returns floats with fractional parts with more than one decimal place" do
       decimal_parts =
         Enum.map(0..10, fn _ ->
@@ -87,18 +96,22 @@ defmodule CaptainsLogTest do
   end
 
   describe "format_stardate" do
+    @task_id 4
     test "returns a string" do
       assert is_bitstring(CaptainsLog.format_stardate(41010.7))
     end
 
+    @task_id 4
     test "formats floats" do
       assert CaptainsLog.format_stardate(41543.3) == "41543.3"
     end
 
+    @task_id 4
     test "rounds floats to one decimal place" do
       assert CaptainsLog.format_stardate(41032.4512) == "41032.5"
     end
 
+    @task_id 4
     test "does not accept integers" do
       assert_raise ArgumentError, fn -> CaptainsLog.format_stardate(41411) end
     end
