8f43529519f17bc3113cdc597357a0736f9d0606
Jie <jie.gillet@gmail.com>
2021-06-11 16:46:14 +0900

    [#723] New practice exercise `zebra-puzzle` (#753)
    * [#723] New practice exercise `zebra-puzzle`
    
    Add solution
    
    Add config file
    
    * Fix typos
    
    * Swap keyword-lists for maps
    
    * Fix formatting
    
    * Fix warnings

---
+++ exercises/practice/zebra-puzzle/test/zebra_puzzle_test.exs
@@ -0,0 +1,14 @@
+defmodule ZebraPuzzleTest do
+  import ZebraPuzzle
+  use ExUnit.Case
+
+  # @tag :pending
+  test "resident who drinks water" do
+    assert ZebraPuzzle.drinks_water() == :norwegian
+  end
+
+  @tag :pending
+  test "resident who owns zebra" do
+    assert ZebraPuzzle.owns_zebra() == :japanese
+  end
+end
