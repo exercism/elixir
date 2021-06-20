ae76d5a25580a9e6377ca9004001eb20fe100bbf
Jie <jie.gillet@gmail.com>
2021-06-20 20:43:42 +0900

    Apply suggestions from code review
    Co-authored-by: Angelika Tyborska <angelikatyborska@fastmail.com>

--- exercises/practice/affine-cipher/lib/affine_cipher.ex
+++ exercises/practice/affine-cipher/lib/affine_cipher.ex
@@ -4,7 +4,7 @@ defmodule AffineCipher do
   """
   @spec encode(key :: %{a: integer, b: integer}, message :: String.t()) ::
           {:ok, String.t()} | {:error, String.t()}
-  def(encode(%{a: a, b: b}, message)) do
+  def encode(%{a: a, b: b}, message) do
   end
 
   @doc """
