8a89d79eaa29e9d320b01314060c9acc3a5b3bb4
Jie <jie.gillet@gmail.com>
2021-06-25 09:51:28 +0900

    Clearer book type
    Co-authored-by: Angelika Tyborska <angelikatyborska@fastmail.com>

--- exercises/practice/book-store/lib/book_store.ex
+++ exercises/practice/book-store/lib/book_store.ex
@@ -1,5 +1,6 @@
 defmodule BookStore do
-  @type book :: integer
+  @typedoc "A book is represented by its number in the 5-book series"
+  @type book :: 1 | 2 | 3 | 4 | 5
 
   @doc """
   Calculate lowest price (in cents) for a shopping basket containing books.
