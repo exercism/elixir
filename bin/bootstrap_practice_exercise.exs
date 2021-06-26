ddbec2bbd8c52deca060e735b2878eee5460867a
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-06-26 14:21:41 +0200

    Update bin/bootstrap_practice_exercise.exs

--- bin/bootstrap_practice_exercise.exs
+++ bin/bootstrap_practice_exercise.exs
@@ -158,31 +158,31 @@ Mix.Generator.create_file("exercises/practice/#{exercise}/.formatter.exs", forma
 # mix.exs
 mix = """
 defmodule #{module}.MixProject do
-use Mix.Project
+  use Mix.Project
 
-def project do
-  [
-    app: :#{exercise_snake_case},
-    version: "0.1.0",
-    # elixir: "~> 1.8",
-    start_permanent: Mix.env() == :prod,
-    deps: deps()
-  ]
-end
+  def project do
+    [
+      app: :#{exercise_snake_case},
+      version: "0.1.0",
+      # elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
 
-# Run "mix help compile.app" to learn about applications.
-def application do
-  [
-    extra_applications: [:logger]
-  ]
-end
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
 
-# Run "mix help deps" to learn about dependencies.
-defp deps do
-  [
-    # {:dep_from_hexpm, "~> 0.3.0"},
-    # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  ]
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
   end
 end
 """
