5f49c6715cb304f9532cb4eeb703e8979634fd0e
Jie <jie.gillet@gmail.com>
2021-06-30 15:08:20 +0900

    <<_::8>> to binary
    Co-authored-by: Tim Austin <tim@neenjaw.com>

--- exercises/practice/variable-length-quantity/.meta/example.ex
+++ exercises/practice/variable-length-quantity/.meta/example.ex
@@ -2,7 +2,7 @@ defmodule VariableLengthQuantity do
   @doc """
   Encode integers into a bitstring of VLQ encoded bytes
   """
-  @spec encode(integers :: [integer]) :: <<_::8>>
+  @spec encode(integers :: [integer]) :: binary
   def encode(integers) when is_list(integers) do
     integers
     |> Enum.map(&encode/1)
@@ -23,7 +23,7 @@ defmodule VariableLengthQuantity do
   @doc """
   Decode a bitstring of VLQ encoded bytes into a series of integers
   """
-  @spec decode(bytes :: <<_::8>>) :: {:ok, [integer]} | {:error, String.t()}
+  @spec decode(bytes :: binary) :: {:ok, [integer]} | {:error, String.t()}
   def decode(bytes, _status \\ :complete, acc \\ 0)
   def decode(<<>>, :complete, _), do: {:ok, []}
   def decode(<<>>, :incomplete, _), do: {:error, "incomplete sequence"}
--- exercises/practice/variable-length-quantity/lib/variable_length_quantity.ex
+++ exercises/practice/variable-length-quantity/lib/variable_length_quantity.ex
@@ -2,14 +2,14 @@ defmodule VariableLengthQuantity do
   @doc """
   Encode integers into a bitstring of VLQ encoded bytes
   """
-  @spec encode(integers :: [integer]) :: <<_::8>>
+  @spec encode(integers :: [integer]) :: binary
   def encode(integers) do
   end
 
   @doc """
   Decode a bitstring of VLQ encoded bytes into a series of integers
   """
-  @spec decode(bytes :: <<_::8>>) :: {:ok, [integer]} | {:error, String.t()}
+  @spec decode(bytes :: binary) :: {:ok, [integer]} | {:error, String.t()}
   def decode(bytes) do
   end
 end
