62f9ce092b7f799b6228442609bb795373d0ba93
Jérémie Gillet <jie.gillet@gmail.com>
2021-06-30 18:39:52 +0900

    New practice exercise `resistor-color-trio`
    New practice exercise
    
    Revert "New practice exercise"
    
    This reverts commit f625dfe3b02ac1c6927f65e69a42ac3902581e13.

---
+++ exercises/practice/resistor-color-trio/lib/resistor_color_trio.ex
@@ -0,0 +1,8 @@
+defmodule ResistorColorTrio do
+  @doc """
+  Calculate the resistance value in ohm or kiloohm from resistor colors
+  """
+  @spec label(colors :: [atom]) :: {integer, atom}
+  def label(colors) do
+  end
+end
