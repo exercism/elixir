c96d3b0e2e7ce355adfc7d8d38187e9374c5bbd8
Jérémie Gillet <jie.gillet@gmail.com>
2021-06-30 10:54:51 +0900

    Rename square_root to sqrt

--- exercises/practice/square-root/.meta/example.ex
+++ exercises/practice/square-root/.meta/example.ex
@@ -2,21 +2,21 @@ defmodule SquareRoot do
   @doc """
   Calculate the integer square root of a positive integer
   """
-  @spec square_root(radicand :: pos_integer) :: pos_integer
-  def square_root(1), do: 1
+  @spec sqrt(radicand :: pos_integer) :: pos_integer
+  def sqrt(1), do: 1
 
-  def square_root(radicand) do
+  def sqrt(radicand) do
     guess = div(radicand, 2)
-    square_root(radicand, guess)
+    sqrt(radicand, guess)
   end
 
-  def square_root(radicand, guess) do
+  def sqrt(radicand, guess) do
     new_guess = div(guess + div(radicand, guess), 2)
 
     if new_guess == guess do
       guess
     else
-      square_root(radicand, new_guess)
+      sqrt(radicand, new_guess)
     end
   end
 end
--- exercises/practice/square-root/lib/square_root.ex
+++ exercises/practice/square-root/lib/square_root.ex
@@ -2,7 +2,7 @@ defmodule SquareRoot do
   @doc """
   Calculate the integer square root of a positive integer
   """
-  @spec square_root(radicand :: pos_integer) :: pos_integer
-  def square_root(radicand) do
+  @spec sqrt(radicand :: pos_integer) :: pos_integer
+  def sqrt(radicand) do
   end
 end
