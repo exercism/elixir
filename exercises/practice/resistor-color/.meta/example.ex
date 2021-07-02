d6b019e5ce6d79788a74a7d0ad2c3cd5bf075a36
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-02 18:23:31 +0200

    Simplify resistor-color (#807)

--- exercises/practice/resistor-color/.meta/example.ex
+++ exercises/practice/resistor-color/.meta/example.ex
@@ -1,26 +1,20 @@
 defmodule ResistorColor do
-  @colors [
-    :black,
-    :brown,
-    :red,
-    :orange,
-    :yellow,
-    :green,
-    :blue,
-    :violet,
-    :grey,
-    :white
-  ]
-
-  @doc """
-  Return a list of all colors
-  """
-  @spec colors() :: list(atom)
-  def colors, do: @colors
+  @colors %{
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
 
   @doc """
   Return the value of a color band
   """
   @spec code(atom) :: integer()
-  def code(color), do: Enum.find_index(@colors, &(&1 == color))
+  def code(color), do: @colors[color]
 end
