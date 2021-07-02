58faf287cdb7940ea6821eea679d4a5214f55d6a
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-02 15:37:22 +0200

    Simplify resistor-color

--- exercises/practice/resistor-color/lib/resistor_color.ex
+++ exercises/practice/resistor-color/lib/resistor_color.ex
@@ -1,12 +1,5 @@
 defmodule ResistorColor do
   @doc """
-  Return a list of all colors
-  """
-  @spec colors() :: list(atom)
-  def colors do
-  end
-
-  @doc """
   Return the value of a color band
   """
   @spec code(atom) :: integer()
