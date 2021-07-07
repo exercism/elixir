70954fd4df5f6a7d473ece1301ef18951fa5caa9
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 12:17:16 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible.

--- exercises/practice/hello-world/lib/hello_world.ex
+++ exercises/practice/hello-world/lib/hello_world.ex
@@ -2,8 +2,7 @@ defmodule HelloWorld do
   @doc """
   Simply returns "Hello, World!"
   """
-  @spec hello :: String.t()
   def hello do
-    "Your implementation goes here"
+    "Goodbye, Mars!"
   end
 end
