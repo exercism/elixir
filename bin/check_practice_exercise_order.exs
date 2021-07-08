f6a962bf3b7d8dbc9188fc322161c11e18e6de2e
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-08 18:31:41 +0200

    Add order check

---
+++ bin/check_practice_exercise_order.exs
@@ -0,0 +1,44 @@
+Mix.install([
+  {:jason, "~> 1.2"}
+])
+
+{opts, _} = OptionParser.parse!(
+  System.argv(),
+  [strict: [write: :boolean], aliases: [w: :write]]
+)
+
+config =
+  "./config.json"
+  |> File.read!()
+  |> Jason.decode!()
+
+sort_by =
+  fn exercise ->
+    {exercise["difficulty"], exercise["name"]}
+  end
+
+reordered_config =
+  put_in(config, ["exercises", "practice"],
+    Enum.sort_by(config["exercises"]["practice"], sort_by)
+  )
+
+desired_exercise_order =
+  reordered_config
+  |> get_in(["exercises", "practice"])
+  |> Enum.map(& &1["slug"])
+
+actual_exercise_order =
+  config
+  |> get_in(["exercises", "practice"])
+  |> Enum.map(& &1["slug"])
+
+if opts[:write] do
+  # TODO
+else
+  if desired_exercise_order === actual_exercise_order do
+    IO.write("Practice exercises are ordered #{IO.ANSI.green()}✔#{IO.ANSI.reset()}\n")
+  else
+    IO.write("Practice exercises are not ordered #{IO.ANSI.red()}⨯#{IO.ANSI.reset()}\n")
+    IO.write("Rerun this script with --write to fix that.\n")
+  end
+end
