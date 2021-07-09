733d83af65b324f43496ad0c3ce876e75db536c3
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-09 14:00:13 +0200

    Add custom mechanism to overwrite alphabetical order

--- bin/check_practice_exercise_order.exs
+++ bin/check_practice_exercise_order.exs
@@ -9,13 +9,15 @@ Mix.install([
     aliases: [w: :write]
   )
 
+max_custom_order = 999
+
 config =
   "./config.json"
   |> File.read!()
   |> Jason.decode!()
 
 sort_by = fn exercise ->
-  {exercise["difficulty"], exercise["name"]}
+  {exercise["difficulty"], exercise["order"] || max_custom_order, exercise["slug"]}
 end
 
 reordered_config =
@@ -36,13 +38,31 @@ actual_exercise_order =
   |> Enum.map(& &1["slug"])
 
 if opts[:write] do
+  # set order to a value if missing because otherwise it's null and null comes before 1
+  {file_with_order_field, 0} =
+    System.cmd("jq", [
+      ".exercises.practice[] |= if .order==null then . + {order: #{max_custom_order + 1}} else . + {order: .order} end",
+      "config.json"
+    ])
+
+  File.write!("config.json", file_with_order_field)
+
+  # reorder exercises based on difficulty, custom order overwrites, and slug
   {reordered_file, 0} =
-    System.cmd("jq", [".exercises.practice|=sort_by(.difficulty, .name)", "config.json"])
+    System.cmd("jq", [".exercises.practice |= sort_by(.difficulty, .order, .slug)", "config.json"])
 
   File.write!("config.json", reordered_file)
+
+  # remove fields added in the first step
+  {reordered_without_order_field_file, 0} =
+    System.cmd("jq", [
+      ".exercises.practice[] |= if .order > #{max_custom_order} then del(.order) else . end",
+      "config.json"
+    ])
+
+  File.write!("config.json", reordered_without_order_field_file)
 else
-  if desired_exercise_order === actual_exercise_order &&
-       Enum.at(desired_exercise_order, 0) == "hello-world" do
+  if desired_exercise_order === actual_exercise_order do
     IO.write("Practice exercises are ordered #{IO.ANSI.green()}✔#{IO.ANSI.reset()}\n")
   else
     IO.write("Practice exercises are not ordered #{IO.ANSI.red()}⨯#{IO.ANSI.reset()}\n")
