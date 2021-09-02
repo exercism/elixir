7b35e31ad0a6e4e4314a77ea4cdb700f184999ce
Michał Łępicki <michallepicki@users.noreply.github.com>
2021-09-02 09:12:18 +0200

    Freelancer rates: Assert discount result with tolerance (#858)

--- exercises/concept/freelancer-rates/test/freelancer_rates_test.exs
+++ exercises/concept/freelancer-rates/test/freelancer_rates_test.exs
@@ -31,7 +31,7 @@ defmodule FreelancerRatesTest do
 
     @tag task_id: 2
     test "it doesn't round" do
-      assert FreelancerRates.apply_discount(111.11, 13.5) == 96.11015
+      assert_in_delta FreelancerRates.apply_discount(111.11, 13.5), 96.11015, 0.000001
     end
   end
 
