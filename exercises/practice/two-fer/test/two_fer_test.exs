d5f3834445cacf93ec7bfa9048013c21dce4c65a
Maurizio Pasquinelli <pasquinellimaurizio@gmail.com>
2021-09-06 10:19:22 +0200

    End two-fer test sentences with dot (#877)
    * End two-fer test sentences with dot
    
    * End two-fer sentence in example.ex with dot

--- exercises/practice/two-fer/test/two_fer_test.exs
+++ exercises/practice/two-fer/test/two_fer_test.exs
@@ -2,17 +2,17 @@ defmodule TwoFerTest do
   use ExUnit.Case
 
   test "no name given" do
-    assert TwoFer.two_fer() == "One for you, one for me"
+    assert TwoFer.two_fer() == "One for you, one for me."
   end
 
   @tag :pending
   test "a name given" do
-    assert TwoFer.two_fer("Alice") == "One for Alice, one for me"
+    assert TwoFer.two_fer("Alice") == "One for Alice, one for me."
   end
 
   @tag :pending
   test "another name given" do
-    assert TwoFer.two_fer("Bob") == "One for Bob, one for me"
+    assert TwoFer.two_fer("Bob") == "One for Bob, one for me."
   end
 
   @tag :pending
