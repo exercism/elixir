d3eb5cca4d18b8390fa765d2b9b5b265615f5389
CJMAXiK <cjmaxik@users.noreply.github.com>
2021-09-22 13:54:07 +0300

    Add more tests in `rpn-calculator` concept (#921)

--- exercises/concept/rpn-calculator/test/rpn_calculator_test.exs
+++ exercises/concept/rpn-calculator/test/rpn_calculator_test.exs
@@ -2,10 +2,21 @@ defmodule RPNCalculatorTest do
   use ExUnit.Case
 
   @tag task_id: 1
+  test "calculate! returns an :ok atom" do
+    assert RPNCalculator.calculate!([], fn _ -> :ok end) == :ok
+  end
+
+  @tag task_id: 1
   test "let it crash" do
     assert_raise(RuntimeError, fn ->
       RPNCalculator.calculate!([], fn _ -> raise "test error" end)
     end)
+  end
+
+  @tag task_id: 2
+  test "calculate returns a tuple" do
+    assert RPNCalculator.calculate([], fn _ -> "operation completed" end) ==
+             {:ok, "operation completed"}
   end
 
   @tag task_id: 2
@@ -14,8 +25,14 @@ defmodule RPNCalculatorTest do
   end
 
   @tag task_id: 3
+  test "calculate_verbose returns a tuple" do
+    assert RPNCalculator.calculate_verbose([], fn _ -> "operation completed" end) ==
+             {:ok, "operation completed"}
+  end
+
+  @tag task_id: 3
   test "rescue the crash, get error tuple with message" do
-    assert RPNCalculator.calculate_verbose([], fn _ -> raise "test error" end) ==
+    assert RPNCalculator.calculate_verbose([], fn _ -> raise ArgumentError, "test error" end) ==
              {:error, "test error"}
   end
 end
