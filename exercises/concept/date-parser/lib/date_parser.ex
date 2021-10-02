e5ed90bb2562d07fa21f121fff242b3bd04293b3
Cohen Carlisle <cohencarlisle+github@pm.me>
2021-10-02 18:22:05 -0400

    Revert "mix format"
    This reverts commit 09bcf554b7e1f58f3e4955056df27a2df739c2a7.

--- exercises/concept/date-parser/lib/date_parser.ex
+++ exercises/concept/date-parser/lib/date_parser.ex
@@ -1,81 +1,65 @@
 defmodule DateParser do
   def day() do
-    # Please implement the day/0 function
-    ""
+    "" # Please implement the day/0 function
   end
 
   def month() do
-    # Please implement the month/0 function
-    ""
+    "" # Please implement the month/0 function
   end
 
   def year() do
-    # Please implement the year/0 function
-    ""
+    "" # Please implement the year/0 function
   end
 
   def day_names() do
-    # Please implement the day_names/0 function
-    ""
+    "" # Please implement the day_names/0 function
   end
 
   def month_names() do
-    # Please implement the month_names/0 function
-    ""
+    "" # Please implement the month_names/0 function
   end
 
   def capture_day() do
-    # Please implement the capture_day/0 function
-    ""
+    "" # Please implement the capture_day/0 function
   end
 
   def capture_month() do
-    # Please implement the capture_month/0 function
-    ""
+    "" # Please implement the capture_month/0 function
   end
 
   def capture_year() do
-    # Please implement the capture_year/0 function
-    ""
+    "" # Please implement the capture_year/0 function
   end
 
   def capture_day_name() do
-    # Please implement the capture_day_name/0 function
-    ""
+    "" # Please implement the capture_day_name/0 function
   end
 
   def capture_month_name() do
-    # Please implement the capture_month_name/0 function
-    ""
+    "" # Please implement the capture_month_name/0 function
   end
 
   def capture_numeric_date() do
-    # Please implement the capture_numeric_date/0 function
-    ""
+    "" # Please implement the capture_numeric_date/0 function
   end
 
   def capture_month_name_date() do
-    # Please implement the capture_month_name_date/0 function
-    ""
+    "" # Please implement the capture_month_name_date/0 function
   end
 
   def capture_day_month_name_date() do
-    # Please implement the capture_day_month_name_date/0 function
-    ""
+    "" # Please implement the capture_day_month_name_date/0 function
   end
 
   def match_numeric_date() do
-    # Please implement the match_numeric_date/0 function
-    ~r//
+    ~r// # Please implement the match_numeric_date/0 function
   end
 
   def match_month_name_date() do
-    # Please implement the match_month_name_day/0 function
-    ~r//
+    ~r// # Please implement the match_month_name_day/0 function
   end
 
   def match_day_month_name_date() do
-    # Please implement the match_day_month_name_date/0 function
-    ~r//
+    ~r// # Please implement the match_day_month_name_date/0 function
   end
 end
