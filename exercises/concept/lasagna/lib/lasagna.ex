328ab5781ffc5f5e6d626ba00fd24fc80efd28ee
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-10-16 16:41:01 +0200

    Replace "TODO:" with "please" (#968)

--- exercises/concept/lasagna/lib/lasagna.ex
+++ exercises/concept/lasagna/lib/lasagna.ex
@@ -1,11 +1,11 @@
 defmodule Lasagna do
-  # TODO: define the 'expected_minutes_in_oven/0' function
+  # Please define the 'expected_minutes_in_oven/0' function
 
-  # TODO: define the 'remaining_minutes_in_oven/1' function
+  # Please define the 'remaining_minutes_in_oven/1' function
 
-  # TODO: define the 'preparation_time_in_minutes/1' function
+  # Please define the 'preparation_time_in_minutes/1' function
 
-  # TODO: define the 'total_time_in_minutes/2' function
+  # Please define the 'total_time_in_minutes/2' function
 
-  # TODO: define the 'alarm/0' function
+  # Please define the 'alarm/0' function
 end
