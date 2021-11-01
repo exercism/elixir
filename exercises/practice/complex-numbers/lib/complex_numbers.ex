633e9447e6c062812f33e43dafc325498c44fb0f
Jérémie Gillet <jie.gillet@gmail.com>
2021-11-01 13:47:14 +0900

    Bring complex-numbers up to date with canonical-data

--- exercises/practice/complex-numbers/lib/complex_numbers.ex
+++ exercises/practice/complex-numbers/lib/complex_numbers.ex
@@ -1,6 +1,6 @@
 defmodule ComplexNumbers do
   @typedoc """
-  In this module, complex numbers are represented as a tuple-pair containing the real and
+  In this module, complex numbers, or a real and a complex number are represented as a tuple-pair containing the real and
   imaginary parts.
   For example, the real number `1` is `{1, 0}`, the imaginary number `i` is `{0, 1}` and
   the complex number `4+3i` is `{4, 3}'.
@@ -22,30 +22,30 @@ defmodule ComplexNumbers do
   end
 
   @doc """
-  Multiply two complex numbers
+  Multiply two complex numbers, or a real and a complex number
   """
-  @spec mul(a :: complex, b :: complex) :: complex
+  @spec mul(a :: complex | float, b :: complex | float) :: complex
   def mul(a, b) do
   end
 
   @doc """
-  Add two complex numbers
+  Add two complex numbers, or a real and a complex number
   """
-  @spec add(a :: complex, b :: complex) :: complex
+  @spec add(a :: complex | float, b :: complex | float) :: complex
   def add(a, b) do
   end
 
   @doc """
-  Subtract two complex numbers
+  Subtract two complex numbers, or a real and a complex number
   """
-  @spec sub(a :: complex, b :: complex) :: complex
+  @spec sub(a :: complex | float, b :: complex | float) :: complex
   def sub(a, b) do
   end
 
   @doc """
-  Divide two complex numbers
+  Divide two complex numbers, or a real and a complex number
   """
-  @spec div(a :: complex, b :: complex) :: complex
+  @spec div(a :: complex | float, b :: complex | float) :: complex
   def div(a, b) do
   end
 
