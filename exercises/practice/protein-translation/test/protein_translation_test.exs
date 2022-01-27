7e11b35e0762475b5f4d82303cc37b9736e5c735
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-01-27 16:46:29 +0100

    Sync protein-translation (#1039)

--- exercises/practice/protein-translation/test/protein_translation_test.exs
+++ exercises/practice/protein-translation/test/protein_translation_test.exs
@@ -52,16 +52,49 @@ defmodule ProteinTranslationTest do
     end
 
     @tag :pending
-    test "invalid codon" do
-      assert ProteinTranslation.of_codon("INVALID") == {:error, "invalid codon"}
+    test "incomplete codon" do
+      assert ProteinTranslation.of_codon("UG") == {:error, "invalid codon"}
+    end
+
+    @tag :pending
+    test "too long, invalid codon" do
+      assert ProteinTranslation.of_codon("UGGG") == {:error, "invalid codon"}
+    end
+
+    @tag :pending
+    test "known amino acids, but invalid codon" do
+      assert ProteinTranslation.of_codon("AAA") == {:error, "invalid codon"}
+    end
+
+    @tag :pending
+    test "unknown amino acids, not part of a codon" do
+      assert ProteinTranslation.of_codon("XYZ") == {:error, "invalid codon"}
     end
   end
 
   describe "of_rna" do
     @tag :pending
+    test "empty RNA sequence results in no proteins" do
+      strand = ""
+      assert ProteinTranslation.of_rna(strand) == {:ok, []}
+    end
+
+    @tag :pending
     test "translates rna strand into correct protein" do
       strand = "AUGUUUUGG"
       assert ProteinTranslation.of_rna(strand) == {:ok, ~w(Methionine Phenylalanine Tryptophan)}
+    end
+
+    @tag :pending
+    test "sequence of two identical protein codons translates into two identical proteins" do
+      strand = "UUUUUU"
+      assert ProteinTranslation.of_rna(strand) == {:ok, ~w(Phenylalanine Phenylalanine)}
+    end
+
+    @tag :pending
+    test "sequence of two different protein codons translates into two identical proteins" do
+      strand = "UUAUUG"
+      assert ProteinTranslation.of_rna(strand) == {:ok, ~w(Leucine Leucine)}
     end
 
     @tag :pending
@@ -95,13 +128,39 @@ defmodule ProteinTranslationTest do
     end
 
     @tag :pending
-    test "invalid RNA" do
-      assert ProteinTranslation.of_rna("CARROT") == {:error, "invalid RNA"}
+    test "incomplete codon, invalid RNA" do
+      strand = "UG"
+      assert ProteinTranslation.of_rna(strand) == {:error, "invalid RNA"}
+    end
+
+    @tag :pending
+    test "known amino acids, but invalid codon, invalid RNA" do
+      strand = "AAA"
+      assert ProteinTranslation.of_rna(strand) == {:error, "invalid RNA"}
+    end
+
+    @tag :pending
+    test "unknown amino acids, not part of a codon, invalid RNA" do
+      strand = "XYZ"
+      assert ProteinTranslation.of_rna("XYZ") == {:error, "invalid RNA"}
     end
 
     @tag :pending
     test "invalid codon at end of RNA" do
-      assert ProteinTranslation.of_rna("UUUROT") == {:error, "invalid RNA"}
+      strand = "UUUROT"
+      assert ProteinTranslation.of_rna(strand) == {:error, "invalid RNA"}
+    end
+
+    @tag :pending
+    test "incomplete RNA" do
+      strand = "AUGU"
+      assert ProteinTranslation.of_rna(strand) == {:error, "invalid RNA"}
+    end
+
+    @tag :pending
+    test "incomplete RNA valid until a STOP codon" do
+      strand = "UUCUUCUAAUGGU"
+      assert ProteinTranslation.of_rna(strand) == {:ok, ~w(Phenylalanine Phenylalanine)}
     end
   end
 end
