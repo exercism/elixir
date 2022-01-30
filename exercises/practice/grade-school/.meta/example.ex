63566ad8f3c7246b467aeb12c046a1063d1ce30d
Jérémie Gillet <jie.gillet@gmail.com>
2022-01-30 23:31:20 +0900

    Sync: re-write grade-school

--- exercises/practice/grade-school/.meta/example.ex
+++ exercises/practice/grade-school/.meta/example.ex
@@ -1,19 +1,48 @@
 defmodule School do
-  def add(db, name, grade) do
-    if name in List.flatten(Map.values(db)) do
-      db
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @type school :: map
+
+  @doc """
+  Create a new, empty school.
+  """
+  @spec new() :: school
+  def new(), do: %{}
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(school, String.t(), integer) :: {:ok | :error, school}
+  def add(school, name, grade) do
+    if name in List.flatten(Map.values(school)) do
+      {:error, school}
     else
-      Map.update(db, grade, [name], &[name | &1])
+      {:ok, Map.update(school, grade, [name], &[name | &1])}
     end
   end
 
-  def grade(db, grade) do
-    Map.get(db, grade, [])
+  @doc """
+  Return the names of the students in a particular grade, sorted alphabetically.
+  """
+  @spec grade(school, integer) :: [String.t()]
+  def grade(school, grade) do
+    Map.get(school, grade, [])
+    |> Enum.sort()
   end
 
-  def sort(db) do
-    db
-    |> Enum.map(fn {k, v} -> {k, Enum.sort(v)} end)
+  @doc """
+  Return the names of all the students in the school sorted by grade and name.
+  """
+  @spec roster(school) :: [String.t()]
+  def roster(school) do
+    school
+    |> Enum.map(fn {grade, students} -> {grade, Enum.sort(students)} end)
     |> Enum.sort()
+    |> Enum.map(fn {_grade, students} -> students end)
+    |> Enum.concat()
   end
 end
