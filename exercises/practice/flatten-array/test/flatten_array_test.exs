7604ab5ec5c50f126071e7888b8b505ce7295b5f
AH WEI <88667293+kwchang0831@users.noreply.github.com>
2022-02-08 02:23:23 +0800

    Update: `flatten-array` add additional test cases (#1060)

--- exercises/practice/flatten-array/test/flatten_array_test.exs
+++ exercises/practice/flatten-array/test/flatten_array_test.exs
@@ -44,6 +44,18 @@ defmodule FlattenArrayTest do
   end
 
   @tag :pending
+  test "consecutive nil values at the front of the list are omitted from the final result" do
+    assert FlattenArray.flatten([nil, nil, 3]) ==
+             [3]
+  end
+
+  @tag :pending
+  test "consecutive nil values in the middle of the list are omitted from the final result" do
+    assert FlattenArray.flatten([1, nil, nil, 4]) ==
+             [1, 4]
+  end
+
+  @tag :pending
   test "6 level nesting with nil values" do
     assert FlattenArray.flatten([0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2]) ==
              [0, 2, 2, 3, 8, 100, -2]
