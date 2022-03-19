16118c5a87dc8728ff59c863ad8f4693109239dc
rikroos <81434586+rikroos@users.noreply.github.com>
2022-03-19 22:57:37 +0100

    Update example.ex

--- exercises/practice/darts/.meta/example.ex
+++ exercises/practice/darts/.meta/example.ex
@@ -4,7 +4,7 @@ defmodule Darts do
   @doc """
   Calculate the score of a single dart hitting a target
   """
-  @spec score(position :: position) :: integer
+  @spec score(position) :: integer
   def score({x, y}) do
     radius = :math.sqrt(x * x + y * y)
 
