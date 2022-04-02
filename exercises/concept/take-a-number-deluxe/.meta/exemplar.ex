b2d9ffad7d31f053e0e2009e6cdae9093df7fcba
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-04-02 15:13:05 +0200

    Add specs to the boilerplate

--- exercises/concept/take-a-number-deluxe/.meta/exemplar.ex
+++ exercises/concept/take-a-number-deluxe/.meta/exemplar.ex
@@ -3,22 +3,27 @@ defmodule TakeANumberDeluxe do
 
   # Client API
 
+  @spec start_link(keyword()) :: {:ok, pid()}
   def start_link(init_arg) do
     GenServer.start_link(__MODULE__, init_arg)
   end
 
+  @spec report_state(pid()) :: TakeANumberDeluxe.State.t()
   def report_state(machine) do
     GenServer.call(machine, :report_state)
   end
 
+  @spec queue_new_number(pid()) :: {:ok, integer()} | {:error, atom()}
   def queue_new_number(machine) do
     GenServer.call(machine, :queue_new_number)
   end
 
+  @spec serve_next_queued_number(pid(), integer() | nil) :: {:ok, integer()} | {:error, atom()}
   def serve_next_queued_number(machine, priority_number \\ nil) do
     GenServer.call(machine, {:serve_next_queued_number, priority_number})
   end
 
+  @spec reset_state(pid()) :: :ok
   def reset_state(machine) do
     GenServer.cast(machine, :reset_state)
   end
