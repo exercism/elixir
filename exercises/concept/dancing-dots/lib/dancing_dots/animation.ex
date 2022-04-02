0f706358a75d1c04e7959455304343babc401a4b
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-04-02 10:25:53 +0200

    Fix indentation

--- exercises/concept/dancing-dots/lib/dancing_dots/animation.ex
+++ exercises/concept/dancing-dots/lib/dancing_dots/animation.ex
@@ -1,16 +1,16 @@
- defmodule DancingDots.Animation do
+defmodule DancingDots.Animation do
   @type dot :: DancingDots.Dot.t()
   @type opts :: keyword
   @type error :: any
   @type frame_number :: pos_integer
 
   # Please implement the module
- end
+end
 
- defmodule DancingDots.Flicker do
+defmodule DancingDots.Flicker do
   # Please implement the module
- end
+end
 
- defmodule DancingDots.Zoom do
+defmodule DancingDots.Zoom do
   # Please implement the module
- end
+end
