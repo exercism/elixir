a5d7fa5844e5f150fbaf2be0f69ebad8cefd6f0b
Jérémie Gillet <jie.gillet@gmail.com>
2022-04-15 22:21:15 +0900

    New practice exercise `killer-sudoku-helper`

---
+++ exercises/practice/killer-sudoku-helper/lib/killer_sudoku_helper.ex
@@ -0,0 +1,8 @@
+defmodule KillerSudokuHelper do
+  @doc """
+  Return the possible combinations of `size` distinct numbers from 1-9 excluding `exclude` that sum up to `sum`.
+  """
+  @spec combinations(cage :: %{exclude: [integer], size: integer, sum: integer}) :: [[integer]]
+  def combinations(cage) do
+  end
+end
