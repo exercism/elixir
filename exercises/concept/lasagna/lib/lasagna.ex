f1a48deed05865c2bc2dee467f8c82c672406681
João Vitor Martins Araújo <87587551+JVMartyns@users.noreply.github.com>
2022-05-29 14:51:29 -0300

    Update lasagna.ex
    My solution

--- exercises/concept/lasagna/lib/lasagna.ex
+++ exercises/concept/lasagna/lib/lasagna.ex
@@ -1,11 +1,16 @@
 defmodule Lasagna do
   # Please define the 'expected_minutes_in_oven/0' function
-
+  def expected_minutes_in_oven, do: 40
   # Please define the 'remaining_minutes_in_oven/1' function
-
+  def remaining_minutes_in_oven(minutes) do
+    expected_minutes_in_oven - minutes
+  end
   # Please define the 'preparation_time_in_minutes/1' function
-
+  def preparation_time_in_minutes(layers), do: layers * 2
   # Please define the 'total_time_in_minutes/2' function
-
+  def total_time_in_minutes(layers, minutes) do
+    preparation_time_in_minutes(layers) + minutes
+  end
   # Please define the 'alarm/0' function
+  def alarm, do: "Ding!"
 end
