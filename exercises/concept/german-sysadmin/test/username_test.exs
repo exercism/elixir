0ce71fcd7e7305dcc38238002337ce1dddbb05d1
Paul Moore <pdmoore@gmail.com>
2022-08-22 20:09:16 -0700

    Update username_test.exs
    - Added a simpler test for lowercase retention that matches the example from the README
    - Renamed the test that keeps lowercase to indicate it also keeps Latin and German
    - Switched the order of the asserts so you get the information that the counts do not match before the strings are compared (this would have helped me a lot)
    - Used the 'allows' language from the README in the underscores test

--- exercises/concept/german-sysadmin/test/username_test.exs
+++ exercises/concept/german-sysadmin/test/username_test.exs
@@ -8,22 +8,27 @@ defmodule UsernameTest do
     end
 
     @tag task_id: 1
-    test "it keeps lowercase latin letters" do
+    test "it removes everything but lowercase letters" do
+      assert Username.sanitize('schmidt1985') == 'schmidt'
+    end
+
+    @tag task_id: 1
+    test "it keeps all lowercase Latin letters (and ignores German letters that require special handling)" do
       input = Enum.to_list(0..0x10FFFF) -- [?_, ?ß, ?ä, ?ö, ?ü]
       expected = 'abcdefghijklmnopqrstuvwxyz'
       actual = Username.sanitize(input)
 
-      assert Enum.take(actual, Enum.count(expected)) == expected
       assert Enum.count(actual) == Enum.count(expected)
+      assert Enum.take(actual, Enum.count(expected)) == expected
     end
 
     @tag task_id: 2
-    test "it keeps underscores" do
+    test "it allows underscores" do
       assert Username.sanitize('marcel_huber') == 'marcel_huber'
     end
 
     @tag task_id: 3
-    test "it substitutes german letters" do
+    test "it substitutes German letters" do
       assert Username.sanitize('krüger') == 'krueger'
       assert Username.sanitize('köhler') == 'koehler'
       assert Username.sanitize('jäger') == 'jaeger'
