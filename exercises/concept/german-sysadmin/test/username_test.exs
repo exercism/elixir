527d72b31d9eff42a94bbd7aca7ff2910c8a60cd
Paul Moore <pdmoore@gmail.com>
2022-08-23 23:37:29 -0700

    Add more test cases to german-sysadmin (#1182)

--- exercises/concept/german-sysadmin/test/username_test.exs
+++ exercises/concept/german-sysadmin/test/username_test.exs
@@ -8,22 +8,47 @@ defmodule UsernameTest do
     end
 
     @tag task_id: 1
-    test "it keeps lowercase latin letters" do
-      input = Enum.to_list(0..0x10FFFF) -- [?_, ?ß, ?ä, ?ö, ?ü]
-      expected = 'abcdefghijklmnopqrstuvwxyz'
-      actual = Username.sanitize(input)
+    test "it allows lowercase Latin letters" do
+      assert Username.sanitize('anne') == 'anne'
+    end
 
-      assert Enum.take(actual, Enum.count(expected)) == expected
-      assert Enum.count(actual) == Enum.count(expected)
+    @tag task_id: 1
+    test "it allows the whole lowercase Latin alphabet" do
+      lowercase_latin_letters = 'abcdefghijklmnopqrstuvwxyz'
+
+      assert Username.sanitize(lowercase_latin_letters) == lowercase_latin_letters
+    end
+
+    @tag task_id: 1
+    test "it removes numbers" do
+      assert Username.sanitize('schmidt1985') == 'schmidt'
+    end
+
+    @tag task_id: 1
+    test "it removes punctuation" do
+      assert Username.sanitize('*fritz*!$%') == 'fritz'
+    end
+
+    @tag task_id: 1
+    test "it removes whitespace" do
+      assert Username.sanitize(' olaf ') == 'olaf'
+    end
+
+    @tag task_id: 1
+    test "it removes all disallowed characters" do
+      allowed_characters = 'abcdefghijklmnopqrstuvwxyz_ßäöü'
+      input = Enum.to_list(0..0x10FFFF) -- allowed_characters
+
+      assert Username.sanitize(input) == ''
     end
 
     @tag task_id: 2
-    test "it keeps underscores" do
+    test "it allows underscores" do
       assert Username.sanitize('marcel_huber') == 'marcel_huber'
     end
 
     @tag task_id: 3
-    test "it substitutes german letters" do
+    test "it substitutes German letters" do
       assert Username.sanitize('krüger') == 'krueger'
       assert Username.sanitize('köhler') == 'koehler'
       assert Username.sanitize('jäger') == 'jaeger'
