3cad93b1b0f49286d6cb5456bfef38a0339c33f4
Felix <15047524+mafeka@users.noreply.github.com>
2023-07-11 14:34:24 +0200

    Update exemplar.ex

--- exercises/concept/pacman-rules/.meta/exemplar.ex
+++ exercises/concept/pacman-rules/.meta/exemplar.ex
@@ -12,6 +12,6 @@ defmodule Rules do
   end
 
   def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?) do
-    has_eaten_all_dots and not lose?(power_pellet_active?, touching_ghost?)
+    has_eaten_all_dots? and not lose?(power_pellet_active?, touching_ghost?)
   end
 end
