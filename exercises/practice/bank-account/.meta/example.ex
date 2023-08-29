c5f47b28c9ef3911194f35196a6217e0000a823f
serhiy-yevtushenko <syevtushenko@gmail.com>
2023-08-29 18:55:46 +0200

    Update example.ex
    Added extended type description to balance method

--- exercises/practice/bank-account/.meta/example.ex
+++ exercises/practice/bank-account/.meta/example.ex
@@ -73,7 +73,7 @@ defmodule BankAccount do
   @doc """
   Get the account's balance.
   """
-  @spec balance(account) :: integer
+  @spec balance(account) :: integer | {:error, :account_closed}
   def balance(account) do
     if Process.alive?(account) do
       GenServer.call(account, :balance)
