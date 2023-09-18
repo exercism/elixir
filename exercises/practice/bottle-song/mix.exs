dcd13cca3323e4291d9a84bbae6c0a9e100f37c7
Antoine Duchenet <antoine.duchenet@gmail.com>
2023-09-18 14:10:20 +0200

    chore(bottle-song): lower Elixir version requirement meta in `mix.exs`
    And bump the version to `1.0.0`.

--- exercises/practice/bottle-song/mix.exs
+++ exercises/practice/bottle-song/mix.exs
@@ -4,8 +4,8 @@ defmodule BottleSong.MixProject do
   def project do
     [
       app: :bottle_song,
-      version: "0.1.0",
-      elixir: "~> 1.15",
+      version: "1.0.0",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
