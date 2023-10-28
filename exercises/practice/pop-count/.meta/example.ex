05a92fa6251ed1e1c788620ec4534613f0cdc14d
Kah Goh <villastar@yahoo.com.au>
2023-10-28 21:41:50 +0800

    Add Pop Count practice exercise (#1385)
    Co-authored-by: Angelika Tyborska <angelikatyborska@fastmail.com>

---
+++ exercises/practice/pop-count/.meta/example.ex
@@ -0,0 +1,17 @@
+import Bitwise
+
+defmodule PopCount do
+  @doc """
+  Given the number, count the number of eggs.
+  """
+  @spec egg_count(number :: integer()) :: non_neg_integer()
+  def egg_count(number) do
+    do_count(number, 0)
+  end
+
+  defp do_count(0, acc), do: acc
+
+  defp do_count(number, acc) when number > 0 do
+    do_count(number >>> 1, acc + (number &&& 1))
+  end
+end
---
+++ exercises/practice/pop-count/lib/pop_count.ex
@@ -0,0 +1,8 @@
+defmodule PopCount do
+  @doc """
+  Given the number, count the number of eggs.
+  """
+  @spec egg_count(number :: integer()) :: non_neg_integer()
+  def egg_count(number) do
+  end
+end
