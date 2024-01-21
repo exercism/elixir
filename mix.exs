3d2b76812f3050524918adb1f16cd2367eb95f9d
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-01-21 12:37:32 +0100

    Switch to hex

--- mix.exs
+++ mix.exs
@@ -25,10 +25,7 @@ defmodule ExercismTestRunner.Mixfile do
   defp deps do
     [
       {:dialyxir, "~> 1.3.0", runtime: false},
-      {:markdown_code_block_formatter,
-       git: "https://github.com/angelikatyborska/markdown_code_block_formatter",
-       tag: "5b6de6ac6621cab2c9bd4abc9d9797dd2e9846f8",
-       runtime: false}
+      {:markdown_code_block_formatter, "~> 0.1.0", runtime: false}
     ]
   end
 end
