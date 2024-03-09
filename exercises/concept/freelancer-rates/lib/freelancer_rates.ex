727f6e60c0fdfeff16599ccf2cd3668c930e0d8b
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-09 14:12:59 +0530

    feat: Update freelancer_rates.ex
    Signed-off-by: harish-datla <36021808+harish-datla@users.noreply.github.com>

--- exercises/concept/freelancer-rates/lib/freelancer_rates.ex
+++ exercises/concept/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,17 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    8.0*hourly_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount*(100-discount)*(1/100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    ceil(apply_discount(8*22*hourly_rate, discount))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    Float.floor(budget/apply_discount(daily_rate(hourly_rate), discount),1)
   end
 end
