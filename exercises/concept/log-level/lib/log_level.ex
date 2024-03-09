f2545baa0dbf0a34c2e489b2ef10f7f71c4bb6c4
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-09 15:34:12 +0530

    feat: Update log_level.ex
    Signed-off-by: harish-datla <36021808+harish-datla@users.noreply.github.com>

--- exercises/concept/log-level/lib/log_level.ex
+++ exercises/concept/log-level/lib/log_level.ex
@@ -1,9 +1,23 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level,legacy?)
+    cond do
+      label == :error || label == :fatal -> :ops
+      label == :unknown && legacy? -> :dev1
+      label == :unknown -> :dev2
+      true -> false
+    end
   end
 end
