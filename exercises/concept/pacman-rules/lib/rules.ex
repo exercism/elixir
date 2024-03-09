ff4b78f66c2ce5fc876e321112b87df522bdad33
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-09 13:36:20 +0530

    feat: Update  pacman rules

--- exercises/concept/pacman-rules/lib/rules.ex
+++ exercises/concept/pacman-rules/lib/rules.ex
@@ -1,17 +1,14 @@
 defmodule Rules do
   def eat_ghost?(power_pellet_active?, touching_ghost?) do
-    # Please implement the eat_ghost?/2 function
+    power_pellet_active? and touching_ghost?
   end
-
   def score?(touching_power_pellet?, touching_dot?) do
-    # Please implement the score?/2 function
+    touching_power_pellet? or touching_dot?
   end
-
   def lose?(power_pellet_active?, touching_ghost?) do
-    # Please implement the lose?/2 function
+    not power_pellet_active? and touching_ghost?
   end
-
   def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?) do
-    # Please implement the win?/3 function
+    has_eaten_all_dots? and not lose?(power_pellet_active?, touching_ghost?)
   end
 end
