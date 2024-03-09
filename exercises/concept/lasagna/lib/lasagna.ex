a36ad2b0050237083bf656987855b6cbcc148916
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-09 13:20:39 +0530

    feat: Update lasagna.ex

--- exercises/concept/lasagna/lib/lasagna.ex
+++ exercises/concept/lasagna/lib/lasagna.ex
@@ -1,11 +1,7 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
-
-  # Please define the 'remaining_minutes_in_oven/1' function
-
-  # Please define the 'preparation_time_in_minutes/1' function
-
-  # Please define the 'total_time_in_minutes/2' function
-
-  # Please define the 'alarm/0' function
+  def expected_minutes_in_oven(), do: 40
+  def remaining_minutes_in_oven(time_spent), do: expected_minutes_in_oven() - time_spent
+  def preparation_time_in_minutes(layers), do: 2*layers
+  def total_time_in_minutes(layers,time_spent), do: preparation_time_in_minutes(layers) + time_spent
+  def alarm(), do: "Ding!"
 end
