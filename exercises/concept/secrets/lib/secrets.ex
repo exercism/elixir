6b793ddb9ac78497139b93e64877d851d8ef55cc
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-09 15:06:31 +0530

    deat: Update secrets.ex
    Signed-off-by: harish-datla <36021808+harish-datla@users.noreply.github.com>

--- exercises/concept/secrets/lib/secrets.ex
+++ exercises/concept/secrets/lib/secrets.ex
@@ -1,29 +1,29 @@
 defmodule Secrets do
   def secret_add(secret) do
-    # Please implement the secret_add/1 function
+    &(&1 + secret)
   end
 
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    &(&1 - secret)
   end
 
   def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
+    &(&1*secret)
   end
 
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    &(Integer.floor_div(&1,secret))
   end
 
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    &(Bitwise.band(&1,secret))
   end
 
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    &(Bitwise.bxor(&1,secret))
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    &(secret_function2.(secret_function1.(&1)))
   end
 end
