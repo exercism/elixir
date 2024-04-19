2c453d58c562239dd8449955b1629fe059e1de33
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-04-19 14:30:14 +0200

    Fix types in complex numbers

--- exercises/practice/complex-numbers/.meta/example.ex
+++ exercises/practice/complex-numbers/.meta/example.ex
@@ -5,45 +5,45 @@ defmodule ComplexNumbers do
   For example, the real number `1` is `{1, 0}`, the imaginary number `i` is `{0, 1}` and
   the complex number `4+3i` is `{4, 3}'.
   """
-  @type complex :: {float, float}
+  @type complex :: {number, number}
 
   @doc """
   Return the real part of a complex number
   """
-  @spec real(a :: complex) :: float
+  @spec real(a :: complex) :: number
   def real({real, _im}), do: real
 
   @doc """
   Return the imaginary part of a complex number
   """
-  @spec imaginary(a :: complex) :: float
+  @spec imaginary(a :: complex) :: number
   def imaginary({_real, im}), do: im
 
   @doc """
   Multiply two complex numbers, or a real and a complex number
   """
-  @spec mul(a :: complex | float, b :: complex | float) :: complex
+  @spec mul(a :: complex | number, b :: complex | number) :: complex
   def mul({a, b}, {c, d}), do: {a * c - b * d, b * c + a * d}
   def mul(a, b), do: mul(to_complex(a), to_complex(b))
 
   @doc """
   Add two complex numbers, or a real and a complex number
   """
-  @spec add(a :: complex | float, b :: complex | float) :: complex
+  @spec add(a :: complex | number, b :: complex | number) :: complex
   def add({a, b}, {c, d}), do: {a + c, b + d}
   def add(a, b), do: add(to_complex(a), to_complex(b))
 
   @doc """
   Subtract two complex numbers, or a real and a complex number
   """
-  @spec sub(a :: complex | float, b :: complex | float) :: complex
+  @spec sub(a :: complex | number, b :: complex | number) :: complex
   def sub({a, b}, {c, d}), do: {a - c, b - d}
   def sub(a, b), do: sub(to_complex(a), to_complex(b))
 
   @doc """
   Divide two complex numbers, or a real and a complex number
   """
-  @spec div(a :: complex | float, b :: complex | float) :: complex
+  @spec div(a :: complex | number, b :: complex | number) :: complex
   def div({a, b}, {c, d}) do
     {(a * c + b * d) / (c * c + d * d), (b * c - a * d) / (c * c + d * d)}
   end
--- exercises/practice/complex-numbers/lib/complex_numbers.ex
+++ exercises/practice/complex-numbers/lib/complex_numbers.ex
@@ -5,47 +5,47 @@ defmodule ComplexNumbers do
   For example, the real number `1` is `{1, 0}`, the imaginary number `i` is `{0, 1}` and
   the complex number `4+3i` is `{4, 3}'.
   """
-  @type complex :: {float, float}
+  @type complex :: {number, number}
 
   @doc """
   Return the real part of a complex number
   """
-  @spec real(a :: complex) :: float
+  @spec real(a :: complex) :: number
   def real(a) do
   end
 
   @doc """
   Return the imaginary part of a complex number
   """
-  @spec imaginary(a :: complex) :: float
+  @spec imaginary(a :: complex) :: number
   def imaginary(a) do
   end
 
   @doc """
   Multiply two complex numbers, or a real and a complex number
   """
-  @spec mul(a :: complex | float, b :: complex | float) :: complex
+  @spec mul(a :: complex | number, b :: complex | number) :: complex
   def mul(a, b) do
   end
 
   @doc """
   Add two complex numbers, or a real and a complex number
   """
-  @spec add(a :: complex | float, b :: complex | float) :: complex
+  @spec add(a :: complex | number, b :: complex | number) :: complex
   def add(a, b) do
   end
 
   @doc """
   Subtract two complex numbers, or a real and a complex number
   """
-  @spec sub(a :: complex | float, b :: complex | float) :: complex
+  @spec sub(a :: complex | number, b :: complex | number) :: complex
   def sub(a, b) do
   end
 
   @doc """
   Divide two complex numbers, or a real and a complex number
   """
-  @spec div(a :: complex | float, b :: complex | float) :: complex
+  @spec div(a :: complex | number, b :: complex | number) :: complex
   def div(a, b) do
   end
 
