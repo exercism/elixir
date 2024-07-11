4c2e075b2cd547190adfefd5d6e548e2105db4b7
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-07-11 19:30:06 +0200

    Reformat sneaky string

--- bin/bootstrap_practice_exercise.exs
+++ bin/bootstrap_practice_exercise.exs
@@ -167,7 +167,7 @@ defmodule #{module}.MixProject do
     [
       app: :#{exercise_snake_case},
       version: "0.1.0",
-            start_permanent: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
