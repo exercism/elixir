13814c6e577c40c7d9abf94eec2fed66ae787f66
Jeremie Gillet <jie.gillet@gmail.com>
2025-04-04 09:15:37 +0900

    finish up with updatet canonical data

--- exercises/practice/relative-distance/.docs/instructions.md
+++ exercises/practice/relative-distance/.docs/instructions.md
@@ -31,4 +31,9 @@ The degree of separation between Tariq and Uma is 3 (Tariq → Helena → Isla �
 There's no known relationship between Isla and [Kevin][six-bacons], as there is no connection in the given data.
 The degree of separation between Uma and Isla is 1.
 
+~~~~exercism/note
+Isla and Tariq are siblings and have a separation of 1.
+Similarly, this implementation would report a separation of 2 from you to your father's brother.
+~~~~
+
 [six-bacons]: https://en.m.wikipedia.org/wiki/Six_Degrees_of_Kevin_Bacon
--- exercises/practice/relative-distance/.meta/example.ex
+++ exercises/practice/relative-distance/.meta/example.ex
@@ -1,65 +1,42 @@
 defmodule RelativeDistance do
   @doc """
-  TODO: add function description and replace types in @spec
+  Find the degree of separation of two members given a given family tree.
   """
   @spec degree_of_separation(
-          family_tree :: %{
-            ian: TODO.t(),
-            tariq: TODO.t(),
-            umar: TODO.t(),
-            yuki: TODO.t(),
-            viktor: TODO.t(),
-            pedro: TODO.t(),
-            wang: TODO.t(),
-            tomoko: TODO.t(),
-            diego: TODO.t(),
-            elif: TODO.t(),
-            liam: TODO.t(),
-            farah: TODO.t(),
-            jing: TODO.t(),
-            aiko: TODO.t(),
-            uma: TODO.t(),
-            hana: TODO.t(),
-            zara: TODO.t(),
-            zane: TODO.t(),
-            sofia: TODO.t(),
-            boris: TODO.t(),
-            hassan: TODO.t(),
-            aditi: TODO.t(),
-            ravi: TODO.t(),
-            leila: TODO.t(),
-            mateo: TODO.t(),
-            giorgio: TODO.t(),
-            vera: TODO.t(),
-            nia: TODO.t(),
-            olga: TODO.t(),
-            rami: TODO.t(),
-            qi: TODO.t(),
-            bao: TODO.t(),
-            elias: TODO.t(),
-            carlos: TODO.t(),
-            gustavo: TODO.t(),
-            oscar: TODO.t(),
-            isla: TODO.t(),
-            quynh: TODO.t(),
-            khadija: TODO.t(),
-            sven: TODO.t(),
-            javier: TODO.t(),
-            fatima: TODO.t(),
-            xia: TODO.t(),
-            yassin: TODO.t(),
-            wyatt: TODO.t(),
-            dalia: TODO.t(),
-            mina: TODO.t(),
-            noah: TODO.t(),
-            priya: TODO.t(),
-            xiomara: TODO.t(),
-            celine: TODO.t(),
-            kaito: TODO.t()
-          },
-          person_a :: TODO.t(),
-          person_b :: TODO.t()
-        ) :: TODO.t()
+          family_tree :: %{String.t() => [String.t()]},
+          person_a :: String.t(),
+          person_b :: String.t()
+        ) :: nil | pos_integer()
   def degree_of_separation(family_tree, person_a, person_b) do
+    family_tree
+    |> build_family_graph()
+    |> find_separation(person_b, [{person_a, 0}], MapSet.new())
+  end
+
+  defp build_family_graph(family_tree) do
+    for {parent, children} <- family_tree, child <- children, reduce: %{} do
+      graph ->
+        siblings = children |> MapSet.new() |> MapSet.delete(child)
+
+        graph
+        |> Map.update(parent, MapSet.new([child]), &MapSet.put(&1, child))
+        |> Map.update(child, MapSet.new([parent]), &MapSet.put(&1, parent))
+        |> Map.update(child, siblings, &MapSet.union(&1, siblings))
+    end
+  end
+
+  defp find_separation(_graph, _goal, [], _history), do: nil
+
+  defp find_separation(_graph, goal, [{goal, count} | _], _history), do: count
+
+  defp find_separation(graph, goal, [{person, count} | rest], history) do
+    history = MapSet.put(history, person)
+
+    next_steps =
+      graph[person]
+      |> Enum.reject(fn relative -> MapSet.member?(history, relative) end)
+      |> Enum.map(fn relative -> {relative, count + 1} end)
+
+    find_separation(graph, goal, rest ++ next_steps, history)
   end
 end
