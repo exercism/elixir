8170cc778b79594df5d4220f4f34ead33eaaae88
Jeremie Gillet <jie.gillet@gmail.com>
2025-06-13 10:24:58 +0900

    add nils to struct types

--- exercises/practice/piecing-it-together/lib/piecing_it_together.ex
+++ exercises/practice/piecing-it-together/lib/piecing_it_together.ex
@@ -6,13 +6,13 @@ defmodule PiecingItTogether do
   defmodule JigsawPuzzle do
     @type format() :: :landscape | :portrait | :square
     @type t() :: %__MODULE__{
-            pieces: pos_integer(),
-            rows: pos_integer(),
-            columns: pos_integer(),
-            format: format(),
-            aspect_ratio: float(),
-            border: pos_integer(),
-            inside: pos_integer()
+            pieces: pos_integer() | nil,
+            rows: pos_integer() | nil,
+            columns: pos_integer() | nil,
+            format: format() | nil,
+            aspect_ratio: float() | nil,
+            border: pos_integer() | nil,
+            inside: pos_integer() | nil
           }
 
     defstruct [:pieces, :rows, :columns, :format, :aspect_ratio, :border, :inside]
