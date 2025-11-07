9401857a2afb32e29fe467d72735ee8a1a7a1ac6
Angelika Cathor <angelikacathor@fastmail.com>
2025-11-07 09:32:06 +0100

    Upgrade Elixir and dialyxir

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule ExercismTestRunner.Mixfile do
 
   defp deps do
     [
-      {:dialyxir, "~> 1.3.0", runtime: false},
+      {:dialyxir, "~> 1.4.7", runtime: false},
       {:markdown_code_block_formatter, "~> 0.1.0", runtime: false},
       {:doctest_formatter, "~> 0.4.0", runtime: false}
     ]
