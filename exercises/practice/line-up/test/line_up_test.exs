d079887d135b8062b40efd9d339768491975a249
FraSanga <108059916+FraSanga@users.noreply.github.com>
2026-03-09 19:02:15 +0100

    clean-up

--- exercises/practice/line-up/test/line_up_test.exs
+++ exercises/practice/line-up/test/line_up_test.exs
@@ -1,197 +1,98 @@
 defmodule LineUpTest do
   use ExUnit.Case
 
-  # Names are chosen without Unicode characters to keep the exercise simple.
-  #
-  # The tests are ordered starting with small regular cases and coming to
-  # bigger and irregular cases later on.
   # @tag :pending
   test "format smallest non-exceptional ordinal numeral 4" do
-    name = "Gianna"
-    number = 4
-    output = LineUp.format(name, number)
-    expected = "Gianna, you are the 4th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Gianna", 4) == "Gianna, you are the 4th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format greatest single digit non-exceptional ordinal numeral 9" do
-    name = "Maarten"
-    number = 9
-    output = LineUp.format(name, number)
-    expected = "Maarten, you are the 9th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Maarten", 9) == "Maarten, you are the 9th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 5" do
-    name = "Petronila"
-    number = 5
-    output = LineUp.format(name, number)
-    expected = "Petronila, you are the 5th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Petronila", 5) == "Petronila, you are the 5th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 6" do
-    name = "Attakullakulla"
-    number = 6
-    output = LineUp.format(name, number)
-    expected = "Attakullakulla, you are the 6th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Attakullakulla", 6) == "Attakullakulla, you are the 6th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 7" do
-    name = "Kate"
-    number = 7
-    output = LineUp.format(name, number)
-    expected = "Kate, you are the 7th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Kate", 7) == "Kate, you are the 7th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 8" do
-    name = "Maximiliano"
-    number = 8
-    output = LineUp.format(name, number)
-    expected = "Maximiliano, you are the 8th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Maximiliano", 8) == "Maximiliano, you are the 8th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 1" do
-    name = "Mary"
-    number = 1
-    output = LineUp.format(name, number)
-    expected = "Mary, you are the 1st customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Mary", 1) == "Mary, you are the 1st customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 2" do
-    name = "Haruto"
-    number = 2
-    output = LineUp.format(name, number)
-    expected = "Haruto, you are the 2nd customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Haruto", 2) == "Haruto, you are the 2nd customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 3" do
-    name = "Henriette"
-    number = 3
-    output = LineUp.format(name, number)
-    expected = "Henriette, you are the 3rd customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Henriette", 3) == "Henriette, you are the 3rd customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format smallest two digit non-exceptional ordinal numeral 10" do
-    name = "Alvarez"
-    number = 10
-    output = LineUp.format(name, number)
-    expected = "Alvarez, you are the 10th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Alvarez", 10) == "Alvarez, you are the 10th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 11" do
-    name = "Jacqueline"
-    number = 11
-    output = LineUp.format(name, number)
-    expected = "Jacqueline, you are the 11th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Jacqueline", 11) == "Jacqueline, you are the 11th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 12" do
-    name = "Juan"
-    number = 12
-    output = LineUp.format(name, number)
-    expected = "Juan, you are the 12th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Juan", 12) == "Juan, you are the 12th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 13" do
-    name = "Patricia"
-    number = 13
-    output = LineUp.format(name, number)
-    expected = "Patricia, you are the 13th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Patricia", 13) == "Patricia, you are the 13th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 21" do
-    name = "Washi"
-    number = 21
-    output = LineUp.format(name, number)
-    expected = "Washi, you are the 21st customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Washi", 21) == "Washi, you are the 21st customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 62" do
-    name = "Nayra"
-    number = 62
-    output = LineUp.format(name, number)
-    expected = "Nayra, you are the 62nd customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Nayra", 62) == "Nayra, you are the 62nd customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 100" do
-    name = "John"
-    number = 100
-    output = LineUp.format(name, number)
-    expected = "John, you are the 100th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("John", 100) == "John, you are the 100th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 101" do
-    name = "Zeinab"
-    number = 101
-    output = LineUp.format(name, number)
-    expected = "Zeinab, you are the 101st customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Zeinab", 101) == "Zeinab, you are the 101st customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format non-exceptional ordinal numeral 112" do
-    name = "Knud"
-    number = 112
-    output = LineUp.format(name, number)
-    expected = "Knud, you are the 112th customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Knud", 112) == "Knud, you are the 112th customer we serve today. Thank you!"
   end
 
   @tag :pending
   test "format exceptional ordinal numeral 123" do
-    name = "Yma"
-    number = 123
-    output = LineUp.format(name, number)
-    expected = "Yma, you are the 123rd customer we serve today. Thank you!"
-
-    assert output == expected
+    assert LineUp.format("Yma", 123) == "Yma, you are the 123rd customer we serve today. Thank you!"
   end
 end
